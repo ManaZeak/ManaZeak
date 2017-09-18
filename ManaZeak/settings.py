716460d51ae9b7ec1a8d9ef481136967513850cb
Arthur <arthur.beaulieu@free.fr>
2017-09-18 15:39:02 +0200

    Remove webpacK
    Former-commit-id: d8b49263042d2c10d6d94c0de2fc90b6c16b0e76

--- ManaZeak/settings.py
+++ ManaZeak/settings.py
@@ -38,19 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-    'webpack_loader',
 ]
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'assets'),
 )
-
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'bundles/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
