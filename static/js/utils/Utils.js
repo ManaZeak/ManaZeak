b453950ce82b62098e542a4dcf8a676537765b8a
Arthur Beaulieu <contact@abeaulieu.fr>
2017-09-27 11:32:08 +0200

    Moved CONVERT to rescan call in Ks

--- static/js/utils/Utils.js
+++ static/js/utils/Utils.js
@@ -25,16 +25,8 @@ function secondsToTimecode(time) {
     }
 }
 
-function loadJSON(file, callback) {
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', file, true);
-    xobj.onreadystatechange = function() {
-        if (xobj.readyState === 4 && xobj.status === 200) {
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
+function ajaxRequestGET(file, callback) {
+
 }
 
 function getCookies() {
