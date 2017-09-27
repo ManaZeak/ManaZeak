23028442c0a571729dafe32881b1e18ed6fe1429
Arthur Beaulieu <contact@abeaulieu.fr>
2017-09-27 13:32:43 +0200

    Get UI components from Django now working

--- static/js/utils/Utils.js
+++ static/js/utils/Utils.js
@@ -39,3 +39,16 @@ function getCookies() {
 
     return cookies;
 }
+
+function fetchComponentUI(url, callback) {
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            callback(this.responseText);
+        }
+    };
+
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
