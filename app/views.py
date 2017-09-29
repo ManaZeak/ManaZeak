9e8c7a49989ba8c46fa16ab44e3064d0c79edd68
Squadella <pbouniol@et.esiea.fr>
2017-09-29 13:03:08 +0200

    Code refactoring
    Former-commit-id: 36d80fa0d1db0ab5e3c29c94e21b77a45d082fd4

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import re
 
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -13,7 +14,7 @@ from django.views.generic.list import ListView
 from app.controller import addTrackMP3, scanLibrary
 from app.form import UserForm
 from app.models import Playlist, Track, Artist, Album, Library
-from app.utils import badFormatError
+from app.utils import badFormatError, exportPlaylistToJson
 
 
 class mainView(ListView):
@@ -167,117 +168,14 @@ def loadAllLibrary(request):
 
 # Get all track information from a playlist and format it as json
 def loadTrackFromPlaylist(request):
-    finalData = {}
     if request.method == 'POST':
         response = json.loads(request.body)
         try:
             print(response['ID'])
             playlist = Playlist.objects.get(id=response['ID'])
-            tracks = playlist.track.all()
-            # finalData = serialize('json', tracks)
-            finalData = "["
-            for track in tracks:
-                finalData += "{ \"ID\":"
-                finalData += str(track.id)
-                finalData += ", \"TITLE\":\""
-                if track.title is not None:
-                    finalData += track.title
-                else:
-                    finalData += " "
-                finalData += "\", \"YEAR\":"
-                if track.year is not None:
-                    finalData += str(track.year)
-                else:
-                    finalData += "0"
-                finalData += ", \"COMPOSER\":\""
-                if track.composer is not None:
-                    finalData += track.composer
-                else:
-                    finalData += " "
-                finalData += "\", \"PERFORMER\":\""
-                if track.performer is not None:
-                    finalData += track.performer
-                else:
-                    finalData += " "
-                finalData += "\", \"TRACK_NUMBER\":"
-                if track.number is not None:
-                    finalData += str(track.number)
-                else:
-                    finalData += "0"
-                finalData += ", \"BPM\":"
-                if track.bpm is not None:
-                    finalData += str(track.bpm)
-                else:
-                    finalData += "0"
-                finalData += ", \"LYRICS\":\""
-                if track.lyrics is not None:
-                    finalData += track.lyrics
-                else:
-                    finalData += " "
-                finalData += "\", \"COMMENT\":\""
-                if track.comment is not None:
-                    finalData += track.comment
-                else:
-                    finalData += " "
-                finalData += "\", \"BITRATE\":"
-                finalData += str(track.bitRate)
-                finalData += ", \"SAMPLERATE\":"
-                finalData += str(track.sampleRate)
-                finalData += ", \"DURATION\":"
-                finalData += str(track.duration)
-                finalData += ", \"DISC_NUMBER\":"
-                if track.discNumber is not None:
-                    finalData += str(track.discNumber)
-                else:
-                    finalData += "0"
-                finalData += ", \"SIZE\":"
-                if track.size is not None:
-                    finalData += str(track.size)
-                else:
-                    finalData += "0"
-                finalData += ", \"LAST_MODIFIED\":\""
-                finalData += str(track.lastModified)
-                finalData += "\", \"ARTISTS\":["
-                for artist in track.artist.all():
-                    finalData += "{\"ID\":"
-                    finalData += str(artist.id)
-                    finalData += ", \"NAME\":\""
-                    finalData += artist.name
-                    finalData += "\"},"
-                finalData = finalData[:-1]
-                finalData += "], \"ALBUM\": { \"ID\":"
-                finalData += str(track.album.id)
-                finalData += ", \"TITLE\":\""
-                if track.album.title is not None:
-                    finalData += track.album.title
-                else:
-                    finalData += " "
-                finalData += "\", \"NUMBER_OF_DISC\":"
-                if track.album.numberOfDisc is not None:
-                    finalData += str(track.album.numberOfDisc)
-                else:
-                    finalData += "0"
-                finalData += ", \"NUMBER_TOTAL_TRACK\":"
-                if track.album.numberTotalTrack is not None:
-                    finalData += str(track.album.numberTotalTrack)
-                else:
-                    finalData += "0"
-                finalData += ", \"ARTIST\":["
-                for artist in track.album.artist.all():
-                    finalData += "{\"ID\":"
-                    finalData += str(artist.id)
-                    finalData += ", \"NAME\":\""
-                    finalData += artist.name
-                    finalData += "\"},"
-                finalData = finalData[:-1]
-                finalData += "]}},"
-            finalData = finalData[:-1]
-            finalData += "]"
-            print(finalData)
-
+            return HttpResponse(exportPlaylistToJson(playlist))
         except AttributeError:
             badFormatError()
-    return HttpResponse(finalData)
 
 
 # Create a new library
