6f2410f0c0539af652b00e7c4234cf658b4a9d15
Arthur Beaulieu <contact@abeaulieu.fr>
2017-10-01 15:24:46 +0200

    Added ListViewEntry class

--- static/js/view/ListView.js
+++ static/js/view/ListView.js
@@ -8,6 +8,7 @@ var ListView = function(tracks) {
     this.tracks = tracks;
     this.entries = [];
 
+
     this.init();
 };
 
@@ -18,34 +19,34 @@ ListView.prototype = {
         this.listView = mkElem("div");
         this.listView.id ="listView";
 
-        this.initColumnBar();
+        this.initColumnHeader();
 
         this.addTracks(this.tracks);
         getById("mainContainer").appendChild(this.listView);
     },
 
 
-    initColumnBar: function() {
+    initColumnHeader: function() {
         var columnBar = mkElem("div");
         columnBar.className = "columnHeader";
 
+        var duration    = mkElem("div");
         var title    = mkElem("div");
-//        var titleResize    = document.createElement("div");
         var artist   = mkElem("div");
         var composer = mkElem("div");
         var album    = mkElem("div");
         var genre    = mkElem("div");
         var year     = mkElem("div");
 
-        title.className    = "title field";
-//        titleResize.id    = "titleResize";
-//        titleResize.className    = "resize";
-        artist.className   = "artist field";
-        composer.className = "composer field";
-        album.className    = "album field";
-        genre.className    = "genre field";
-        year.className     = "year field";
+        duration.className    = "col-duration field";
+        title.className    = "col-title field";
+        artist.className   = "col-artist field";
+        composer.className = "col-composer field";
+        album.className    = "col-album field";
+        genre.className    = "col-genre field";
+        year.className     = "col-year field";
 
+        duration.innerHTML    = "Duration";
         title.innerHTML    = "Title";
         artist.innerHTML   = "Artist";
         composer.innerHTML = "Composer";
@@ -53,8 +54,8 @@ ListView.prototype = {
         genre.innerHTML    = "Genre";
         year.innerHTML     = "Year";
 
+        columnBar.appendChild(duration);
         columnBar.appendChild(title);
-//        columnBar.appendChild(titleResize);
         columnBar.appendChild(artist);
         columnBar.appendChild(composer);
         columnBar.appendChild(album);
@@ -67,60 +68,7 @@ ListView.prototype = {
 
     addTracks: function(tracks) {
         for (var i = 0; i < tracks.length ;++i) {
-            this.newEntry(tracks[i]);
-        }
-    },
-
-
-    newEntry: function(track) {
-        var entry = mkElem("div");
-        entry.id = track.id.track;
-        entry.className = "trackContainer";
-
-        var title    = mkElem("div");
-        var artist   = mkElem("div");
-        var composer = mkElem("div");
-        var album    = mkElem("div");
-        var genre    = mkElem("div");
-        var year     = mkElem("div");
-
-        title.className    = "title field";
-        artist.className   = "artist field";
-        composer.className = "composer field";
-        album.className    = "album field";
-        genre.className     = "genre field";
-        year.className     = "year field";
-
-        title.innerHTML    = track.title;
-        artist.innerHTML   = track.artist;
-        composer.innerHTML = track.composer;
-        album.innerHTML    = track.album;
-        genre.innerHTML    = track.genre;
-        year.innerHTML     = track.year;
-
-        entry.appendChild(title);
-        entry.appendChild(artist);
-        entry.appendChild(composer);
-        entry.appendChild(album);
-        entry.appendChild(genre);
-        entry.appendChild(year);
-
-        // TODO : store listenners, and add single click listenner
-        entry.addEventListener("dblclick", this.toggleSelected.bind(this));
-
-        this.entries.push(entry);
-        this.listView.appendChild(entry);
-    },
-
-
-    toggleSelected: function() {
-        // TODO : console.log(this.ui.entry.getBoundingClientRect());
-        if (this.isSelected) {
-            this.isSelected = !this.isSelected;
-            this.ui.entry.style.background = "none";
-        } else {
-            this.isSelected = !this.isSelected;
-            this.ui.entry.style.background = "red";
+            this.entries.push(new ListViewEntry(tracks[i], this.listView));
         }
     }
 };
