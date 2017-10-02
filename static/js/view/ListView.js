9a93d3e92e907dcd60623f773aa25fab70419085
Arthur Beaulieu <contact@abeaulieu.fr>
2017-10-02 21:04:08 +0200

    Sorting system on ListView implemented

--- static/js/view/ListView.js
+++ static/js/view/ListView.js
@@ -8,6 +8,29 @@ var ListView = function(tracks) {
     this.tracks = tracks;
     this.entries = [];
 
+    this.header = {
+        duration:  null,
+        title:     null,
+        artist:    null,
+        composer:  null,
+        performer: null,
+        album:     null,
+        genre:     null,
+        bitRate:   null,
+        year:      null
+    };
+
+    this.sort = {
+        isDurationAsc:  false,
+        isTitleAsc:     false,
+        isArtistAsc:    false,
+        isComposerAsc:  false,
+        isPerformerAsc: false,
+        isAlbumAsc:     false,
+        isGenreAsc:     false,
+        isBitRateAsc:   false,
+        isYearAsc:      false
+    };
 
     this.init();
 };
@@ -20,8 +43,9 @@ ListView.prototype = {
         this.listView.id ="listView";
 
         this.initColumnHeader();
+        this._eventListener();
 
-        this.addTracks(this.tracks);
+        this.addEntries(this.tracks);
         getById("mainContainer").appendChild(this.listView);
     },
 
@@ -30,53 +54,127 @@ ListView.prototype = {
         var columnBar = mkElem("div");
         columnBar.className = "columnHeader";
 
-        var duration        = mkElem("div");
-        var title           = mkElem("div");
-        var artist          = mkElem("div");
-        var composer        = mkElem("div");
-        var performer       = mkElem("div");
-        var album           = mkElem("div");
-        var genre           = mkElem("div");
-        var bitRate         = mkElem("div");
-        var year            = mkElem("div");
+        this.header.duration  = mkElem("div");
+        this.header.title     = mkElem("div");
+        this.header.artist    = mkElem("div");
+        this.header.composer  = mkElem("div");
+        this.header.performer = mkElem("div");
+        this.header.album     = mkElem("div");
+        this.header.genre     = mkElem("div");
+        this.header.bitRate   = mkElem("div");
+        this.header.year      = mkElem("div");
 
-        duration.className  = "col-duration";
-        title.className     = "col-title";
-        artist.className    = "col-artist";
-        composer.className  = "col-composer";
-        performer.className = "col-performer";
-        album.className     = "col-album";
-        genre.className     = "col-genre";
-        bitRate.className   = "col-bitRate";
-        year.className      = "col-year";
+        this.header.duration.className    = "col-duration";
+        this.header.title.className       = "col-title";
+        this.header.artist.className      = "col-artist";
+        this.header.composer.className    = "col-composer";
+        this.header.performer.className   = "col-performer";
+        this.header.album.className       = "col-album";
+        this.header.genre.className       = "col-genre";
+        this.header.bitRate.className     = "col-bitRate";
+        this.header.year.className        = "col-year";
 
-        duration.innerHTML  = "Duration";
-        title.innerHTML     = "Title";
-        artist.innerHTML    = "Artist";
-        composer.innerHTML  = "Composer";
-        performer.innerHTML = "Performer";
-        album.innerHTML     = "Album";
-        genre.innerHTML     = "Genre";
-        bitRate.innerHTML   = "BitRate";
-        year.innerHTML      = "Year";
+        this.header.duration.innerHTML    = "Duration";
+        this.header.title.innerHTML       = "Title";
+        this.header.artist.innerHTML      = "Artist";
+        this.header.composer.innerHTML    = "Composer";
+        this.header.performer.innerHTML   = "Performer";
+        this.header.album.innerHTML       = "Album";
+        this.header.genre.innerHTML       = "Genre";
+        this.header.bitRate.innerHTML     = "BitRate";
+        this.header.year.innerHTML        = "Year";
 
-        columnBar.appendChild(duration);
-        columnBar.appendChild(title);
-        columnBar.appendChild(artist);
-        columnBar.appendChild(composer);
-        columnBar.appendChild(performer);
-        columnBar.appendChild(album);
-        columnBar.appendChild(genre);
-        columnBar.appendChild(bitRate);
-        columnBar.appendChild(year);
+        columnBar.appendChild(this.header.duration);
+        columnBar.appendChild(this.header.title);
+        columnBar.appendChild(this.header.artist);
+        columnBar.appendChild(this.header.composer);
+        columnBar.appendChild(this.header.performer);
+        columnBar.appendChild(this.header.album);
+        columnBar.appendChild(this.header.genre);
+        columnBar.appendChild(this.header.bitRate);
+        columnBar.appendChild(this.header.year);
 
         this.listView.appendChild(columnBar);
     },
 
 
-    addTracks: function(tracks) {
+    addEntries: function(tracks) {
         for (var i = 0; i < tracks.length ;++i) {
             this.entries.push(new ListViewEntry(tracks[i], this.listView));
         }
+    },
+
+
+    removeEntries: function() {
+        for (var i = 0; i < this.entries.length ;++i) {
+            this.listView.removeChild(this.entries[i].entry)
+        }
+
+        this.entries = [];
+    },
+
+
+    sortBy: function(attribute, ascending) {
+        this.removeEntries();
+        this.tracks.sort(sortBy(attribute, ascending));
+        this.addEntries(this.tracks);
+    },
+
+
+    _eventListener: function() {
+        var that = this;
+
+        this.header.duration.addEventListener("click", function() {
+            that.sort.isDurationAsc = !that.sort.isDurationAsc;
+            that.sortBy("duration", that.sort.isDurationAsc);
+        });
+
+        this.header.title.addEventListener("click", function() {
+            that.sort.isTitleAsc = !that.sort.isTitleAsc;
+            that.sortBy("title", that.sort.isTitleAsc);
+        });
+
+        this.header.artist.addEventListener("click", function() {
+            that.sort.isArtistAsc = !that.sort.isArtistAsc;
+            that.sortBy("artist", that.sort.isArtistAsc);
+        });
+
+        this.header.composer.addEventListener("click", function() {
+            that.sort.isComposerAsc = !that.sort.isComposerAsc;
+            that.sortBy("composer", that.sort.isComposerAsc);
+        });
+
+        this.header.performer.addEventListener("click", function() {
+            that.sort.isPerformerAsc = !that.sort.isPerformerAsc;
+            that.sortBy("performer", that.sort.isPerformerAsc);
+        });
+
+        this.header.album.addEventListener("click", function() {
+            that.sort.isAlbumAsc = !that.sort.isAlbumAsc;
+            that.sortBy("album", that.sort.isAlbumAsc);
+        });
+
+        this.header.genre.addEventListener("click", function() {
+            that.sort.isGenreAsc = !that.sort.isGenreAsc;
+            that.sortBy("genre", that.sort.isGenreAsc);
+        });
+
+        this.header.bitRate.addEventListener("click", function() {
+            that.sort.isBiteRateAsc = !that.sort.isBiteRateAsc;
+            that.sortBy("bitRate", that.sort.isBiteRateAsc);
+        });
+
+        this.header.year.addEventListener("click", function() {
+            that.sort.isYearAsc = !that.sort.isYearAsc;
+            that.sortBy("year", that.sort.isYearAsc);
+        });
+
     }
 };
+
+/*
+        album: null,
+        genre: null,
+        bitRate: null,
+        year: null
+ */
