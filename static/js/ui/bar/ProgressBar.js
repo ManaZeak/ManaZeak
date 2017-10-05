1dc0395c715c5dfa19e8a1f698429ca0ff978dd6
Arthur Beaulieu <contact@abeaulieu.fr>
2017-10-05 12:36:47 +0200

    Peiro police
    Former-commit-id: 29c0f9813e6ae9d7ee8877b87f3d81e6212bd7e2

--- static/js/ui/bar/ProgressBar.js
+++ static/js/ui/bar/ProgressBar.js
@@ -5,14 +5,14 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 var ProgressBar = function() {
     this.progressBar = {
-        container: getById("progressBar"),
-        current:   getById("progress"),
-        thumb:     getById("progressThumb")
+        container: document.getElementById("progressBar"),
+        current:   document.getElementById("progress"),
+        thumb:     document.getElementById("progressThumb")
     };
     this.duration = {
-        current: getById("currentDuration"),
-        total:   getById("totalDuration"),
-        hover:   getById("progressTimecodeHover")
+        current: document.getElementById("currentDuration"),
+        total:   document.getElementById("totalDuration"),
+        hover:   document.getElementById("progressTimecodeHover")
     };
 
     this.refreshIntervalId = -1;
