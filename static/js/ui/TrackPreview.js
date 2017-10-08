3157c0a19e14b6bd67b767dbccecebba38fa3401
Arthur Beaulieu <contact@abeaulieu.fr>
2017-10-08 18:25:59 +0200

    Add trackPreview class and update info when track is clicked
    Former-commit-id: a82bf5c9ca97320b63151f4f940769a5e313e29c

---
+++ static/js/ui/TrackPreview.js
@@ -0,0 +1,29 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                     *
+ *  TrackPreview class - handle the track info container (left/footbar)                *
+ *                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var TrackPreview = function() {
+    // UI
+    this.ui = {
+        cover: document.getElementById("trackPreviewCover"),
+        title: document.getElementById("trackPreviewTitle"),
+        artist: document.getElementById("trackPreviewArtist"),
+        album: document.getElementById("trackPreviewAlbum"),
+        year: document.getElementById("trackPreviewYear"),
+        genre: document.getElementById("trackPreviewGenre")
+    };
+};
+
+
+TrackPreview.prototype = {
+
+    changeTrack: function(track) {
+        // TODO : handle cover
+        this.ui.title.innerHTML = track.title;
+        this.ui.artist.innerHTML = track.artist;
+        this.ui.album.innerHTML = track.album;
+        this.ui.year.innerHTML = track.year;
+        this.ui.genre.innerHTML = track.genre;
+    }
+};
