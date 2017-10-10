ce388492d564381f1a3d565dc924780d2bf691ec
Arthur Beaulieu <contact@abeaulieu.fr>
2017-10-10 15:28:24 +0200

    Added moodbar first implementation, to be tested
    Former-commit-id: d5b56666f1e5e8bb24228576de006ad799017dd3

--- static/js/view/ListView.js
+++ static/js/view/ListView.js
@@ -154,6 +154,7 @@ ListView.prototype = {
 
                         window.app.trackPreview.setVisible();
                         window.app.trackPreview.changeTrack(that.entries[id].track, cover);
+                        window.app.topbar.changeMoodbar(that.entries[id].entry.id);
                         window.app.player.changeTrack("../" + response.PATH);
                         window.app.player.play();
                     }
