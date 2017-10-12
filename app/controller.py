587ae4962db9542645776d03f8ed32142d214b1e
Squadella <pbouniol@et.esiea.fr>
2017-10-12 13:55:08 +0200

    First implementation, need to be debugged.

--- app/controller.py
+++ app/controller.py
@@ -5,6 +5,7 @@ import threading
 
 from django.http.response import JsonResponse
 from django.utils.html import strip_tags
+from multiprocessing import Process
 from mutagen.id3 import ID3, ID3NoHeaderError
 from mutagen.mp3 import MP3
 
@@ -12,7 +13,7 @@ from app.models import Track, Artist, Album, FileType, Genre
 
 
 # Return a bad format error
-from app.utils import ResponseThread, ScanThread
+from app.utils import ResponseThread, scanLibraryProcess
 
 
 def badFormatError():
@@ -62,8 +63,9 @@ def scanLibrary(library, playlist, convert):
 
     # TODO: if trackPath is null, return an error
     print("indexed all files")
-    scanThread = ScanThread(mp3Files, library, playlist, convert, coverPath)
-    scanThread.run()
+    mp3ID = FileType.objects.get(name="mp3")
+    scanThread = Process(target=scanLibraryProcess, args=(mp3Files, library, playlist, convert, coverPath, mp3ID,))
+    scanThread.start()
     data = {
         'DONE': True,
         'ID': playlist.id,
