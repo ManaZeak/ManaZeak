02f34b08d017e289064eee60257954f74876347a
Valentin PEIRO <peiro@et.esiea.fr>
2017-10-20 14:47:35 +0200

    Finished the new Context Menu system

--- static/js/ui/menu/ContextMenu.js
+++ static/js/ui/menu/ContextMenu.js
@@ -17,15 +17,17 @@ NewContextMenu.prototype = {
 
     _init: function()
     {
-        this.contextMenu = new ContextMenuEntry("", null);
+        this.contextMenu = new ContextMenuEntry("master", "", null);
         this.contextMenu.activate_event_listener();
-        this.contextMenu.add_child(new ContextMenuEntry("TEST_1", null));
-        this.contextMenu.add_child(new ContextMenuEntry("TEST_2", null));
-        this.contextMenu.add_child(new ContextMenuEntry("TEST_3", function(a) {console.log(a);}, 'NON'));
-        this.contextMenu.children[0].add_child(new ContextMenuEntry("TEST_0", function() {console.log('OUI')}));
+        this.contextMenu.add_child(new ContextMenuEntry("1", "TEST_1", null));
+        this.contextMenu.add_child(new ContextMenuEntry("2", "TEST_2", null));
+        this.contextMenu.add_child(new ContextMenuEntry("3", "TEST_3", function(a) {console.log(a);}, 'NON'), "1", true);
+        this.contextMenu.children[0].add_child(new ContextMenuEntry("4", "TEST_0", function() {console.log('OUI')}));
+        this.contextMenu.children[1].add_child(new ContextMenuEntry(null, "TEST_HR", null));
+        this.contextMenu.children[2].set_visible_areas(["footBar"]);
 
         this.element = document.createElement('DIV');
-        this.element.id = "mzk-context-menu-container";
+        this.element.id = "mzk-ctx-wrap";
         this.element.appendChild(this.contextMenu.element);
         document.body.insertBefore(this.element, document.body.firstChild);
 
@@ -59,9 +61,19 @@ NewContextMenu.prototype = {
                 self.element.style.left = "unset";
                 self.element.style.right = (document.documentElement.clientWidth - event.pageX) + "px";
             }
-            self.contextMenu.close_all();
-            self.element.classList.add("mzk-open");
 
+            self.contextMenu.close_all();
+            self.element.className = "";
+
+            var target = event.target;
+            while(target)
+            {
+                if(target.id)
+                    self.element.classList.add("mzk-ctx-include-" + target.id);
+                target = target.parentNode;
+            }
+
+            self.element.classList.add("mzk-ctx-open");
             event.preventDefault();
             event.stopPropagation();
             event.stopImmediatePropagation();
--- static/js/ui/menu/ContextMenuEntry.js
+++ static/js/ui/menu/ContextMenuEntry.js
@@ -3,11 +3,10 @@
  *  NewContextMenu class - handle the context menu on right click                      *
  *                                                                                     *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-var ContextMenuEntry = function (displayString, callback/*, MORE ARGUMENTS HERE*/)
+var ContextMenuEntry = function (entryID, displayString, callback/*, MORE ARGUMENTS HERE*/)
 {
-
     // /!\ IMPORTANT - CHANGE THIS IF YOU ADD/REMOVE ARGUMENTS FROM THIS CONSTRUCTOR /!\
-    var NB_OF_NAMED_ARGS = 2;
+    var NB_OF_NAMED_ARGS = 3;
 
     this.displayString = displayString;
     this.callback = callback;
@@ -16,10 +15,15 @@ var ContextMenuEntry = function (displayString, callback/*, MORE ARGUMENTS HERE*
         this.callback_args[i - NB_OF_NAMED_ARGS] = arguments[i];
 
     this.multi_open_submenu = false;
+    this.entryID = entryID;
+    this.hide_rule = null;
+    this.show_rule = null;
+
     this.element = null;
     this.children = [];
     this.parent = null;
 
+    this.check_stylesheet();
     this._init();
 };
 
@@ -29,6 +33,8 @@ ContextMenuEntry.prototype = {
     _init: function ()
     {
         this.element = document.createElement("UL");
+        if(this.entryID)
+            this.element.id = "mzk-ctx-ul-" + this.entryID;
     },
 
     set_multi_open_submenu: function(allow)
@@ -37,41 +43,74 @@ ContextMenuEntry.prototype = {
         return this;
     },
 
-    add_child: function (other_entry)
+    set_visible_areas: function(array_of_IDs)
     {
-        var hr = document.createElement("HR");
+        if(this.entryID == null)
+            return;
+
+        var menu_selector = "#mzk-ctx-wrap";
+        var this_selector = " #mzk-ctx-li-" + this.entryID;
+        var hide_css = "{ display: none; visibility: hidden; }";
+        var show_css = "{ display: block; visibility: visible; }";
+        var sheet = window.app.cssFiles.contextMenu;
+
+        this.hide_rule = sheet.insertRule(menu_selector + this_selector + hide_css);
+        var show_selector = ".mzk-ctx-void";
+        for(var i = 0; i < array_of_IDs.length; i++)
+            show_selector += "," + menu_selector + ".mzk-ctx-include-" + array_of_IDs[i] + this_selector;
+        this.show_rule = sheet.insertRule(show_selector + show_css);
+        console.dir(sheet);
+    },
+
+    add_child: function (other_entry, before_ID, after)
+    {
+        //Create the child element
         var li = document.createElement("LI");
         li.dataset.parentIx = this.children.length;
+        if (other_entry.entryID)
+            li.id = "mzk-ctx-li-" + other_entry.entryID;
+
         li.textContent = other_entry.displayString;
         li.appendChild(other_entry.element);
 
-        this.element.appendChild(li);
-        this.element.appendChild(hr);
+        //Find where to insert it
+        var childRef = this.find_child_by_ID(before_ID);
+        if (childRef)
+        {
+            if (after == false)
+                childRef = childRef.element.parentNode;
+            else
+                childRef = childRef.element.parentNode.nextSibling;
+        }
+
+        //Insert it and add the cross references
+        this.element.insertBefore(li, childRef);
         this.children.push(other_entry);
         other_entry.parent = this;
 
         if(this.element.parentNode)
-            this.element.parentNode.classList.add("mzk-submenu");
+            this.element.parentNode.classList.add("mzk-ctx-submenu");
     },
 
+    //Call this on the root
     activate_event_listener: function ()
     {
         var self = this;
         this.element.addEventListener("click", function (event)
         {
-            if(event.target.tagName !== 'LI')
+            if(event.target.tagName != 'LI')
                 return true;
 
             var target = event.target;
             var ixArray = new Array(10);
             var i = 0;
 
-            while(target.parentNode !== self.element)
+            while(target.parentNode != self.element)
             {
                 ixArray[i++] = target.dataset.parentIx;
                 do
                     target = target.parentNode;
-                while(target.tagName !== 'LI');
+                while(target.tagName != 'LI');
             }
             ixArray[i] = target.dataset.parentIx;
 
@@ -87,7 +126,7 @@ ContextMenuEntry.prototype = {
             {
                 if(clicked.parent.multi_open_submenu)
                     clicked.parent.close_all();
-                clicked.element.classList.toggle("mzk-open");
+                clicked.element.classList.toggle("mzk-ctx-open");
             }
 
             event.preventDefault();
@@ -96,12 +135,21 @@ ContextMenuEntry.prototype = {
         }, true);
     },
 
+    find_child_by_ID: function(childID) {
+        if(childID == null)
+            return null;
+        for(var i = 0; i < this.children.length; i++)
+            if(this.children[i].entryID === childID)
+                return this.children[i];
+        return null;
+    },
+
     close_all: function()
     {
         this.children.forEach(function(child)
         {
            child.close_all();
-           child.element.classList.remove("mzk-open");
+           child.element.classList.remove("mzk-ctx-open");
         });
     },
 
@@ -109,5 +157,18 @@ ContextMenuEntry.prototype = {
     {
         if (this.callback)
             this.callback.apply(null, this.callback_args);
+    },
+
+    check_stylesheet: function()
+    {
+        if(window.app.cssFiles.contextMenu)
+            return;
+
+        var el = document.createElement("STYLE");
+        //Webkit hack to enable the stylesheet
+        el.appendChild(document.createTextNode(""));
+        var styleSheetIx = document.styleSheets.length;
+        document.head.appendChild(el);
+        window.app.cssFiles.contextMenu = document.styleSheets[styleSheetIx];
     }
 };
