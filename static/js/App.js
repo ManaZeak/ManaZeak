8c0cce86e3cbb23fd91f9df6343f20a2bc2a2f06
Arthur Beaulieu <contact@abeaulieu.fr>
2017-11-21 18:55:44 +0100

    Load all playlist at once

--- static/js/App.js
+++ static/js/App.js
@@ -95,14 +95,14 @@ App.prototype = {
                                                      response,
                                                      undefined));
                     // response = raw tracks JSON object
-//                    for (var i = 1; i < playlists.PLAYLIST_IDS.length; ++i) {
-//                        that.playlists.push(new Playlist(playlists.PLAYLIST_IDS[i],
-//                                                         playlists.PLAYLIST_NAMES[i],
-//                                                         playlists.PLAYLIST_IS_LIBRARY[i],
-//                                                         true,
-//                                                         undefined,
-//                                                         undefined));
-//                    }
+                    for (var i = 1; i < playlists.PLAYLIST_IDS.length; ++i) {
+                        that.playlists.push(new Playlist(playlists.PLAYLIST_IDS[i],
+                                                         playlists.PLAYLIST_NAMES[i],
+                                                         playlists.PLAYLIST_IS_LIBRARY[i],
+                                                         true,
+                                                         undefined,
+                                                         undefined));
+                    }
 
                     that.topBar.init(that.playlists, that.playlists[0]);
                     // TODO : change that.playlists[0] to last ID stored in cookies (0 by default)
