c607879ad6edf83a93dcdb3a33e174aee00cd165
avignat <armand@vignat.org>
2017-12-11 00:24:32 +0100

    Docker improvements

--- docker/moodbar/moodbar_generator.py
+++ docker/moodbar/moodbar_generator.py
@@ -1,96 +1,52 @@
-import hashlib
-import multiprocessing
-import threading
-import time
+#!/usr/bin/env python3
+# coding: utf-8
 
 import os
+from hashlib import md5
+from multiprocessing import Pool
+from os.path import join, getsize
+from time import sleep
 
 
-# Split a table in x tables of equal size
-def splitTable(table, number):
-    if len(table) % number == 0:
-        chunkSize = int(len(table) / number)
-    else:
-        chunkSize = int(len(table) / number) + 1
-    for i in range(0, len(table), chunkSize):
-        yield table[i:i + chunkSize]
+LIBRARY_FOLDER = '/library'
+MOODBAR_FOLDER = '/moodbar/static'
+MAX_PROCESSES = 3
 
 
-class ScanThread(threading.Thread):
-    def __init__(self, files, moods):
-        threading.Thread.__init__(self)
-        self.files = files
-        self.moods = moods
-        self.count = 0
+def generate_moodbar(file):
+    filename_md5 = md5(file.encode("ascii", "ignore")).hexdigest()
+    moodfile_path = join(MOODBAR_FOLDER, '{}.mood'.format(filename_md5))
+    # TODO: Better special chars management (quotes)
+    if not os.path.exists(moodfile_path):
+        os.system('moodbar "{}" -o {}'.format(file, moodfile_path))
 
-    def run(self):
-        for f in self.files:
-            print(f)
-            md5Path = "./static/"
-            md5 = hashlib.md5(f.encode("ascii", "ignore")).hexdigest()
-            md5Path += md5
-            self.count += 1
-            if md5 not in self.moods:
-                command = 'moodbar \"' + f + '\" -o ' + md5Path + '.mood'
-                os.system(command)
 
+def list_songs(path):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith('mp3'):
+                yield join(root, name)
 
 def scan():
-    # TODO: put correct path
-    # TODO: the moodbar docker must have the SAME architecture as the manazeak one.
-    moods = []
-    count = 0
-    print("starting scanning")
-    for root, dirs, files in os.walk("/moodbar/static/"):
-        for file in files:
-            moods.append(file[:-5])
-    filesScanned = []
-    print("starting scanning2")
-    for root, dirs, files in os.walk("/library/"):
-        for file in files:
-            filesScanned.append(os.path.join(root, file))
-    print("starting creating thread")
-    threads = []
-    splicedFiles = splitTable(filesScanned, multiprocessing.cpu_count())
-    for filePaths in splicedFiles:
-        threads.append(ScanThread(filePaths, moods))
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()
-        count += thread.count
-    print("finished scanning")
-    return count
+    # TODO: Display start time and running time
+    print("[SCAN] Starting ...")
 
+    with Pool(processes=MAX_PROCESSES) as p:
+        p.map(generate_moodbar, list_songs(LIBRARY_FOLDER))
 
-def countFile():
-    count = 0
-    for root, dirs, files in os.walk("/library/"):
-        for _ in files:
-            count += 1
-    return count
-
-
-def removeAll():
-    for root, dirs, files in os.walk("/moodbar/static/"):
-        for file in files:
-            path = os.path.join(root, file)
-            os.remove(path)
-
+    print("[SCAN] Done !")
 
 def main():
-    totalFiles = scan()
-    count = 0
-    while True:
-        if totalFiles != countFile():
-            totalFiles = scan()
-        time.sleep(600)
-        count += 1
-        if count == 1008:  # Regenerating all moodbar every week
-            count = 0
-            totalFiles = 0
-            removeAll()
+    print("LIBRARY_FOLDER : {}".format(LIBRARY_FOLDER))
+    print("MOODBAR_FOLDER : {}".format(MOODBAR_FOLDER))
+    print("MAX_PROCESSES  : {}".format(MAX_PROCESSES))
+
+    scan() # Start scan
+
+    while True: # Execute the scan and wait 3600 seconds (1h) before scanning again
+        sleep(3600)
+        scan()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
