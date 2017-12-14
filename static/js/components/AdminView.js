829dc40f71c17cd92d2c30ce104b0cccd53e519f
Arthur Beaulieu <contact@abeaulieu.fr>
2017-12-14 15:35:28 +0100

    Stats WIP w/ sauce11 - Admin crappy design first implem

--- static/js/components/AdminView.js
+++ static/js/components/AdminView.js
@@ -12,10 +12,18 @@ AdminView.prototype = {
 
     _createUI: function(container) {
         this.ui = {
-            container: document.createElement("DIV")
+            container: document.createElement("DIV"),
+            dropLabel: document.createElement("P"),
+            dropButton: document.createElement("BUTTON")
         };
 
         this.ui.container.id = "admin";
+
+        this.ui.dropLabel.innerHTML = "Drop the database";
+        this.ui.dropButton.innerHTML = "DROP";
+
+        this.ui.container.appendChild(this.ui.dropLabel);
+        this.ui.container.appendChild(this.ui.dropButton);
 
         container.appendChild(this.ui.container);
     }
