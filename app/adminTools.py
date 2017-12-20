08e37c73c2ddf3e28c6be8b71fb9dac2ffe0f4c6
Squadella <pbouniol@et.esiea.fr>
2017-12-20 16:36:23 +0100

    First version of automated file name checking.
    Former-commit-id: 5d2b6f5c75772ffbe1503f6c88251fa64bc9bf5e

--- app/adminTools.py
+++ app/adminTools.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
@@ -58,6 +61,60 @@ def dropAllDB(request):
                 data = errorCheckMessage(True, None)
             else:
                 data = errorCheckMessage(False, "permissionError")
+        else:
+            data = errorCheckMessage(False, "permissionError")
+    else:
+        data = errorCheckMessage(False, "badRequest")
+    return JsonResponse(data)
+
+
+@login_required(redirect_field_name='user/login.html', login_url='app:login')
+def checkNamingConventionArtistsOnPlaylist(request):
+    data = {}
+    if request.method == 'POST':
+        user = request.user
+        if user.is_superuser:
+            response = json.loads(request.body)
+            if 'PLAYLIST_ID' in response:
+                playlistId = response['PLAYLIST_ID']
+                data = set()
+                if Playlist.objects.filter(id=playlistId).count() == 1:
+                    tracks = Playlist.objects.get(id=playlistId).track.all()
+                    for track in tracks:
+                        path, fileName = os.path.split(track.location)
+                        splicedName = fileName.split(" - ")
+                        # Extracting artists
+                        artists = splicedName[0]
+                        splicedArtists = artists.split(",")
+                        # Checking if the artists are in a good order
+                        for i in range(len(splicedArtists) - 1):
+                            artist1 = splicedArtists[i].rstrip().lstrip()
+                            artist2 = splicedArtists[i + 1].rstrip().lstrip()
+                            if artist1[0] > artist2[0]:
+                                data.add(track)
+                                break
+
+                        # Checking if the title contains caps at the beginning of each word
+                        fileName = splicedName[1]
+                        words = fileName.split(" ")
+                        for word in words:
+                            if not word[0].isupper():
+                                data.add(track)
+                                break
+
+                        # The tracks contains a featuring
+                        if "(feat." in fileName:
+                            feats = fileName.split("feat.")
+                            for i in range(len(feats) - 1):
+                                artist1 = feats[i].rstrip().lstrip()
+                                artist2 = feats[i + 1].rstrip().lstrip()
+                                if artist1[0] > artist2[0]:
+                                    data.add(track)
+                                    break
+                    data = dict({'RESULT': data})
+                    data = {**data, **errorCheckMessage(True, None)}
+                else:
+                    data = errorCheckMessage(False, "dbError")
         else:
             data = errorCheckMessage(False, "permissionError")
     else:
