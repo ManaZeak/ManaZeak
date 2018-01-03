aa00adbff8c35d9a511e64d49e49102576b2c1f6
Squadella <pbouniol@et.esiea.fr>
2018-01-03 14:40:02 +0100

    Added redirection when the usr does goes on the url /app.
    Former-commit-id: 7dd668bc81d016061299093aafef9f234e4c3d17

--- ManaZeak/urls.py
+++ ManaZeak/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.conf import settings
-from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='/app/', permanent=False), name='basic'),
     url(r'^admin/', admin.site.urls),
     url(r'^app/', include('app.urls')),
 ]
