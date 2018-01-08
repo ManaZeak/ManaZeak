c65d02247ed6aba28a2ec576be73b937b3bd2712
Squadella <pbouniol@et.esiea.fr>
2018-01-08 11:20:19 +0100

    Added options for admin panel.
    Former-commit-id: ce652c1ef9c5ec392d964866d3fb667d9c98444c

--- app/adminTools.py
+++ app/adminTools.py
@@ -1,6 +1,10 @@
+import json
+
+import os
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.utils.html import strip_tags
 
 from app.models import Track, Artist, Album, Playlist, Library, Genre, Shuffle, UserHistory, Stats, History
 from app.utils import errorCheckMessage
@@ -19,6 +23,42 @@ def getAdminView(request):
                 })
             data = dict({'RESULT': userInfo})
             data = {**data, **errorCheckMessage(True, None)}
+        else:
+            data = errorCheckMessage(False, "permissionError")
+    else:
+        data = errorCheckMessage(False, "badRequest")
+    return JsonResponse(data)
+
+
+def removeAllMoods(request):
+    if request.method == 'GET':
+        admin = request.user
+        if admin.is_superuser:
+            moodbars = "/ManaZeak/static/mood/"
+            for mood in os.listdir(moodbars):
+                os.remove(os.path.join(moodbars, mood))
+            data = errorCheckMessage(True, None)
+        else:
+            data = errorCheckMessage(False, "permissionError")
+    else:
+        data = errorCheckMessage(False, "badRequest")
+    return JsonResponse(data)
+
+
+def removeUserById(request):
+    if request.method == 'POST':
+        admin = request.user
+        if admin.is_superuser:
+            response = json.loads(request.body)
+            if 'USER_ID' in response:
+                userId = strip_tags(response['USER_ID'])
+                if User.objects.filter(id=userId).count() == 1:
+                    User.objects.get(id=userId).delete()
+                    data = errorCheckMessage(True, None)
+                else:
+                    data = errorCheckMessage(False, "dbError")
+            else:
+                data = errorCheckMessage(False, "badFormat")
         else:
             data = errorCheckMessage(False, "permissionError")
     else:
