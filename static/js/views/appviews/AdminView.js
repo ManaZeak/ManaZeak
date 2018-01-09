687836a51e8b8cf1a04651b5653e45e9f5eb7067
Arthur Beaulieu <contact@abeaulieu.fr>
2018-01-09 09:20:36 +0100

    Add pages in AdminView, style sill todo
    Former-commit-id: f4775c6631d6745a5af9f7c7a9c2d42782109f31

--- static/js/views/appviews/AdminView.js
+++ static/js/views/appviews/AdminView.js
@@ -10,10 +10,18 @@ class AdminView extends View {
     constructor() {
 
         super();
-        this._createUI();
+        this.info = null;
+        this._init();
     }
 
 //  --------------------------------  PRIVATE METHODS  --------------------------------  //
+
+    _clearPageSpace() {
+        while (this.ui.content.firstChild) {
+            this.ui.content.removeChild(this.ui.content.firstChild);
+        }
+    }
+
 
     /**
      * method : _createUI (private)
@@ -24,38 +32,58 @@ class AdminView extends View {
         this.ui = {
             container: this.container,
             menu:      document.createElement("DIV"),
+            menuTitle: document.createElement("H1"),
 
             menuList:  document.createElement("UL"),
             menuDB:    document.createElement("LI"),
-            menuUser:    document.createElement("LI"),
-            menuLib:    document.createElement("LI"),
+            menuUser:  document.createElement("LI"),
+            menuLib:   document.createElement("LI"),
+            menuSC:    document.createElement("LI"),
 
             content:   document.createElement("DIV"),
+            contentTitle: document.createElement("H1"),
             syncthing: document.createElement("IFRAME")
         };
 
-
-
         this.ui.container.id = "admin";
         this.ui.menu.id      = "leftMenu";
+        this.ui.menuTitle.innerHTML = "Admin";
+
         this.ui.menuDB.innerHTML = "Database";
         this.ui.menuUser.innerHTML = "Users";
         this.ui.menuLib.innerHTML = "Libraries";
-
-        this.ui.menuList.appendChild(this.ui.menuDB);
-        this.ui.menuList.appendChild(this.ui.menuUser);
-        this.ui.menuList.appendChild(this.ui.menuLib);
+        this.ui.menuSC.innerHTML = "SyncThing";
 
         this.ui.content.id   = "content";
- //       this.ui.syncthing.id = "content";
+        //       this.ui.syncthing.id = "content";
 
         //this.ui.syncthing.src = "http://manazeak.org/app";
         //this.ui.syncthing.frameBorder = 0;
 
+        this.ui.menuList.appendChild(this.ui.menuDB);
+        this.ui.menuList.appendChild(this.ui.menuUser);
+        this.ui.menuList.appendChild(this.ui.menuLib);
+        this.ui.menuList.appendChild(this.ui.menuSC);
+
+        this.ui.menu.appendChild(this.ui.menuTitle);
         this.ui.menu.appendChild(this.ui.menuList);
         this.ui.container.appendChild(this.ui.menu);
         this.ui.container.appendChild(this.ui.content);
 
+        this._eventListener();
+        this._requestDBPage();
+    }
+
+
+    _eventListener() {
+        this.ui.menuDB.addEventListener("click", this._requestDBPage.bind(this));
+        this.ui.menuUser.addEventListener("click", this._requestUsersPage.bind(this));
+        this.ui.menuLib.addEventListener("click", this._requestLibrariesPage.bind(this));
+        this.ui.menuSC.addEventListener("click", this._requestSCPage.bind(this));
+    }
+
+
+    _init() {
         let that = this;
         JSONParsedGetRequest(
             "ajax/getAdminView/",
@@ -66,50 +94,110 @@ class AdminView extends View {
                  *     ERROR_MSG : string
                  * } */
                 if (response.DONE) {
-                    that.ui.dropLabel            = document.createElement("P");
-                    that.ui.dropButton           = document.createElement("BUTTON");
-
-                    that.ui.apiKeyLabel            = document.createElement("P");
-                    that.ui.apiKeyField           = document.createElement("INPUT");
-                    that.ui.apiKeyButton           = document.createElement("BUTTON");
-
-                    that.ui.rescanLabel            = document.createElement("P");
-                    that.ui.rescanButton           = document.createElement("BUTTON");
-
-                    that.ui.rmMoodLabel            = document.createElement("P");
-                    that.ui.rmMoodButton           = document.createElement("BUTTON");
-
-                    that.ui.dropLabel.innerHTML  = "Drop the database";
-                    that.ui.dropButton.innerHTML = "DROP";
-
-                    that.ui.apiKeyLabel.innerHTML  = "SyncThing API key";
-                    that.ui.apiKeyField.type = "text";
-                    that.ui.apiKeyField.value = response.SYNC_KEY;
-                    that.ui.apiKeyButton.innerHTML = "SUBMIT";
-
-                    that.ui.rescanLabel.innerHTML  = "Rescan SyncThing folders";
-                    that.ui.rescanButton.innerHTML = "RESCAN";
-
-                    that.ui.rmMoodLabel.innerHTML  = "Remove all moodbar from server"; // TODO : warn user that moodbar will auto gen later
-                    that.ui.rmMoodButton.innerHTML = "REMOVE ALL";
-
-                    that.ui.content.appendChild(that.ui.dropLabel);
-                    that.ui.content.appendChild(that.ui.dropButton);
-                    that.ui.content.appendChild(that.ui.apiKeyLabel);
-                    that.ui.content.appendChild(that.ui.apiKeyField);
-                    that.ui.content.appendChild(that.ui.apiKeyButton);
-                    that.ui.content.appendChild(that.ui.rescanLabel);
-                    that.ui.content.appendChild(that.ui.rescanButton);
-                    that.ui.content.appendChild(that.ui.rmMoodLabel);
-                    that.ui.content.appendChild(that.ui.rmMoodButton);
-
-                    that.ui.dropButton.addEventListener("click", that._requestDrop.bind(that));
-                    that.ui.apiKeyButton.addEventListener("click", that._submitAPIKey.bind(that));
-                    that.ui.rescanButton.addEventListener("click", that._rescanSC.bind(that));
-                    that.ui.rmMoodButton.addEventListener("click", that._removeMoodbar.bind(that));
+                    that.info = response;
+                    that._createUI();
                 }
             }
         );
+    }
+
+
+    _requestDBPage() {
+        this._clearPageSpace();
+
+        this.ui.contentTitle.innerHTML = "Database";
+
+        this.ui.dropLabel            = document.createElement("P");
+        this.ui.dropButton           = document.createElement("BUTTON");
+        this.ui.rmMoodLabel            = document.createElement("P");
+        this.ui.rmMoodButton           = document.createElement("BUTTON");
+
+
+        this.ui.dropLabel.innerHTML  = "Drop the database";
+        this.ui.dropButton.innerHTML = "DROP";
+        this.ui.rmMoodLabel.innerHTML  = "Remove all moodbar from server"; // TODO : warn user that moodbar will auto gen later
+        this.ui.rmMoodButton.innerHTML = "REMOVE ALL";
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(this.ui.dropLabel);
+        this.ui.content.appendChild(this.ui.dropButton);
+        this.ui.content.appendChild(this.ui.rmMoodLabel);
+        this.ui.content.appendChild(this.ui.rmMoodButton);
+
+        this.ui.dropButton.addEventListener("click", this._requestDrop.bind(this));
+        this.ui.rmMoodButton.addEventListener("click", this._removeMoodbar.bind(this));
+    }
+
+
+    _requestUsersPage() {
+        this._clearPageSpace();
+
+        this.ui.contentTitle.innerHTML = "Users";
+
+        let list = document.createElement("UL");
+
+        for (let i = 0; i < this.info.USER.length; ++i) {
+            let admin = this.info.USER[i].ADMIN ? "Admin" : "User";
+            let element = document.createElement("LI");
+            element.innerHTML = this.info.USER[i].NAME + " (ID: " + this.info.USER[i].ID + ") <br>" + admin;
+
+            list.appendChild(element);
+        }
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(list);
+
+        console.log(this.info);
+    }
+
+
+    _requestLibrariesPage() {
+        this._clearPageSpace();
+
+        this.ui.contentTitle.innerHTML = "Libraries";
+
+        let list = document.createElement("UL");
+
+        for (let i = 0; i < this.info.LIBRARIES.length; ++i) {
+            let element = document.createElement("LI");
+            element.innerHTML = this.info.LIBRARIES[i].NAME + " (ID: " + this.info.LIBRARIES[i].ID + ")";
+
+            list.appendChild(element);
+        }
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(list);
+    }
+
+
+    _requestSCPage() {
+        this._clearPageSpace();
+
+        this.ui.contentTitle.innerHTML = "SyncThing";
+
+        this.ui.apiKeyLabel            = document.createElement("P");
+        this.ui.apiKeyField           = document.createElement("INPUT");
+        this.ui.apiKeyButton           = document.createElement("BUTTON");
+        this.ui.rescanLabel            = document.createElement("P");
+        this.ui.rescanButton           = document.createElement("BUTTON");
+
+        this.ui.apiKeyLabel.innerHTML  = "SyncThing API key";
+        this.ui.apiKeyField.type = "text";
+        this.ui.apiKeyField.value = this.info.SYNC_KEY;
+        this.ui.apiKeyButton.innerHTML = "SUBMIT";
+
+        this.ui.rescanLabel.innerHTML  = "Rescan SyncThing folders";
+        this.ui.rescanButton.innerHTML = "RESCAN";
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(this.ui.apiKeyLabel);
+        this.ui.content.appendChild(this.ui.apiKeyField);
+        this.ui.content.appendChild(this.ui.apiKeyButton);
+        this.ui.content.appendChild(this.ui.rescanLabel);
+        this.ui.content.appendChild(this.ui.rescanButton);
+
+        this.ui.apiKeyButton.addEventListener("click", this._submitAPIKey.bind(this));
+        this.ui.rescanButton.addEventListener("click", this._rescanSC.bind(this));
     }
 
 
@@ -201,7 +289,7 @@ class AdminView extends View {
         JSONParsedPostRequest(
             "ajax/changeSyncthingAPIKey/",
             JSON.stringify({
-               SYNC_KEY: this.ui.apiKeyField.value // TODO : Warning, value must be tested
+                SYNC_KEY: this.ui.apiKeyField.value // TODO : Warning, value must be tested
             }),
             function(response) {
                 /* response = {
