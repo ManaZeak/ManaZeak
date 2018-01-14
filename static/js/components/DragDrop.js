f20eb7930a166ea067aa98810aaf26935c0c0c7c
Valentin PEIRO <peiro@et.esiea.fr>
2018-01-14 15:53:33 +0100

    Drag and Drop client display
    Former-commit-id: 4740604526d72aac79474bfee5a74db0e7b91b34

--- static/js/components/DragDrop.js
+++ static/js/components/DragDrop.js
@@ -13,13 +13,29 @@
 class DragDrop {
     constructor(element) {
       this.element = element;
+      this.element.classList.add('mzk-dragdrop');
+      //this.wrapper = document.createElement('DIV');
+      //this.wrapper.className = 'mzk-dragdrop';
 
+      //if(this.element.parentNode)
+      //    this.element.parentNode.replaceChild(this.wrapper, this.element);
+
+      //this.wrapper.appendChild(this.element);
       this._eventListener();
     }
 
     _eventListener() {
+        var self = this;
+        this.element.addEventListener('dragenter', function(event) {
+           self.element.classList.add('mzk-dragdrop-show');
+        });
+        this.element.addEventListener('dragleave', function(event) {
+            if(event.target == self.element)
+               self.element.classList.remove('mzk-dragdrop-show');
+        });
         this.element.addEventListener('drop', function(event) {
             let files = event.dataTransfer.files;
+            self.element.classList.remove('mzk-dragdrop-show');
 
             let f;
             for(let i = 0; i < files.length; i++) {
