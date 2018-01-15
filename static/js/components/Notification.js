bc99e03a60eeb4ecdb47016b1567169f717275e1
Arthur Beaulieu <contact@abeaulieu.fr>
2018-01-15 18:48:28 +0100

    Fix #33

--- static/js/components/Notification.js
+++ static/js/components/Notification.js
@@ -24,7 +24,6 @@ class Notification {
 
         this._createUI();
         this._lifeCycle();
-        console.trace();
     };
 
 //  --------------------------------  PRIVATE METHODS  --------------------------------  //
