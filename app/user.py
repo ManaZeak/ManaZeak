3262df8c239c16850916341822662dd8ae124b4c
Squadella <pbouniol@et.esiea.fr>
2018-01-31 10:59:03 +0100

    Added get user information.

--- app/user.py
+++ app/user.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 
-from app.models import Playlist, UserPreferences, UserHistory
-from app.utils import errorCheckMessage
+from app.models import Playlist, UserPreferences, UserHistory, InviteCode
+from app.utils import errorCheckMessage, populateDB
 
 
 @login_required(redirect_field_name='login.html', login_url='app:login')
@@ -12,6 +12,27 @@ def deleteUser(request):
         deleteLinkedEntities(user)
         user.delete()
         data = errorCheckMessage(True, None)
+    else:
+        data = errorCheckMessage(False, "badRequest")
+    return JsonResponse(data)
+
+
+@login_required(redirect_field_name='login.html', login_url='app:login')
+def getUserInformation(request):
+    if request.method == 'GET':
+        populateDB()
+        user = request.user
+        userPref = UserPreferences.objects.get(user=user)
+        inviteCode = InviteCode.objects.get(user=user).code
+        data = {
+            'USER_ID': user.username,
+            'IS_ADMIN': user.is_superuser,
+            'GROUP_NAME': userPref.group.name,
+            'GROUP_ID': userPref.group.id,
+            'INVITE_CODE': inviteCode,
+            'GODFATHER_CODE': userPref.inviteCodename,
+        }
+        data = {**data, **errorCheckMessage(True, None)}
     else:
         data = errorCheckMessage(False, "badRequest")
     return JsonResponse(data)
