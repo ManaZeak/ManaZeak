a0bd69abaf3ac53c97d3d58f2fa706b34e139dd5
Valentin PEIRO <peiro@et.esiea.fr>
2018-01-31 19:40:52 +0100

    Recovered ListViewEntry from before webpack merge

--- static/js/views/entries/ListViewEntry.js
+++ static/js/views/entries/ListViewEntry.js
@@ -15,46 +15,41 @@ class ListViewEntry {
         this.isSelected      = false;
 
         this.entry           = document.createElement("DIV");
-        let duration         = document.createElement("DIV");
-        let title            = document.createElement("DIV");
-        let artist           = document.createElement("DIV");
-        let composer         = document.createElement("DIV");
-        let performer        = document.createElement("DIV");
-        let album            = document.createElement("DIV");
-        let genre            = document.createElement("DIV");
-        let bitRate          = document.createElement("DIV");
-        let year             = document.createElement("DIV");
+        this.info = {
+            duration: document.createElement("DIV"),
+            title: document.createElement("DIV"),
+            artist: document.createElement("DIV"),
+            composer: document.createElement("DIV"),
+            performer: document.createElement("DIV"),
+            album: document.createElement("DIV"),
+            genre: document.createElement("DIV"),
+            bitRate: document.createElement("DIV"),
+            year: document.createElement("DIV")
+        };
 
         this.entry.className = "trackContainer";
-        duration.className   = "col-duration";
-        title.className      = "col-title";
-        artist.className     = "col-artist";
-        composer.className   = "col-composer";
-        performer.className  = "col-performer";
-        album.className      = "col-album";
-        genre.className      = "col-genre";
-        bitRate.className    = "col-bitRate";
-        year.className       = "col-year";
-        duration.innerHTML   = secondsToTimecode(track.duration);
-        title.innerHTML      = track.title;
-        artist.innerHTML     = track.artist;
-        composer.innerHTML   = track.composer;
-        performer.innerHTML  = track.performer;
-        album.innerHTML      = track.album;
-        genre.innerHTML      = track.genre;
-        bitRate.innerHTML    = Math.round(track.bitRate / 1000) + " kbps";
-        year.innerHTML       = track.year;
+        this.info.duration.className   = "col-duration";
+        this.info.title.className      = "col-title";
+        this.info.artist.className     = "col-artist";
+        this.info.composer.className   = "col-composer";
+        this.info.performer.className  = "col-performer";
+        this.info.album.className      = "col-album";
+        this.info.genre.className      = "col-genre";
+        this.info.bitRate.className    = "col-bitRate";
+        this.info.year.className       = "col-year";
 
-        this.entry.appendChild(duration);
-        this.entry.appendChild(title);
-        this.entry.appendChild(artist);
-        this.entry.appendChild(composer);
-        //this.entry.appendChild(performer);
-        this.entry.appendChild(album);
-        this.entry.appendChild(genre);
-        this.entry.appendChild(bitRate);
-        this.entry.appendChild(year);
+        this.entry.appendChild(this.info.duration);
+        this.entry.appendChild(this.info.title);
+        this.entry.appendChild(this.info.artist);
+        this.entry.appendChild(this.info.composer);
+        //this.entry.appendChild(this.info.performer);
+        this.entry.appendChild(this.info.album);
+        this.entry.appendChild(this.info.genre);
+        this.entry.appendChild(this.info.bitRate);
+        this.entry.appendChild(this.info.year);
 
+        this._setInfo();
+        this._eventListener();
         this.insert(listView);
     }
 
@@ -108,6 +103,33 @@ class ListViewEntry {
         else {
             this.entry.classList.remove("mzk-selected");
         }
+    }
+
+//  -------------------------------  PRIVATE METHODS  ---------------------------------  //
+
+    /**
+     * TODO
+     */
+    _eventListener() {
+        let that = this;
+        this.track.listen('updateMetadata', function() {
+            that._setInfo();
+        });
+    }
+
+    /**
+     * TODO remove zeaz and implement a track bank
+     */
+    _setInfo() {
+        this.info.duration.innerHTML   = secondsToTimecode(this.track.duration);
+        this.info.title.innerHTML      = this.track.title;
+        this.info.artist.innerHTML     = this.track.artist;
+        this.info.composer.innerHTML   = this.track.composer;
+        this.info.performer.innerHTML  = this.track.performer;
+        this.info.album.innerHTML      = this.track.album;
+        this.info.genre.innerHTML      = this.track.genre;
+        this.info.bitRate.innerHTML    = Math.round(this.track.bitRate / 1000) + " kbps";
+        this.info.year.innerHTML       = this.track.year;
     }
 
 //  ------------------------------  GETTERS / SETTERS  --------------------------------  //
