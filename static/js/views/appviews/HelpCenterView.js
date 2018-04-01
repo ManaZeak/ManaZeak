160a049035c76c237a094c6cf90eaa1f63938b23
Arthur Beaulieu <contact@abeaulieu.fr>
2018-04-01 14:55:49 +0200

    Add entries for helpcenter

--- static/js/views/appviews/HelpCenterView.js
+++ static/js/views/appviews/HelpCenterView.js
@@ -42,27 +42,36 @@ class HelpCenterView extends View {
             menu:         document.createElement("DIV"),
             menuTitle:    document.createElement("H2"),
 
-            menuList:     document.createElement("UL"),
-            menuGen:       document.createElement("LI"),
+            menuList:          document.createElement("UL"),
+            menuShortcut:      document.createElement("LI"),
+            menuTagConvention: document.createElement("LI"),
+            menuRanksPerms: document.createElement("LI"),
+            menuManaCoin: document.createElement("LI"),
 
             content:      document.createElement("DIV"),
             contentTitle: document.createElement("H1"),
         };
 
         this.ui.container.classList.add("mzk-user-view");
-        this.ui.menu.className      = "mzk-left-menu";
-        this.ui.content.className   = "mzk-content";
-        this.ui.menuTitle.innerHTML = "Help Center";
-        this.ui.menuGen.innerHTML   = "General";
+        this.ui.menu.className         = "mzk-left-menu";
+        this.ui.content.className      = "mzk-content";
+        this.ui.menuTitle.innerHTML    = "Help Center";
+        this.ui.menuShortcut.innerHTML = "Shortcuts";
+        this.ui.menuTagConvention.innerHTML = "Tag convention";
+        this.ui.menuRanksPerms.innerHTML = "Ranks / Permissions";
+        this.ui.menuManaCoin.innerHTML = "ManaCoin";
 
-        this.ui.menuList.appendChild(this.ui.menuGen);
+        this.ui.menuList.appendChild(this.ui.menuShortcut);
+        this.ui.menuList.appendChild(this.ui.menuTagConvention);
+        this.ui.menuList.appendChild(this.ui.menuRanksPerms);
+        this.ui.menuList.appendChild(this.ui.menuManaCoin);
         this.ui.menu.appendChild(this.ui.menuTitle);
         this.ui.menu.appendChild(this.ui.menuList);
         this.ui.container.appendChild(this.ui.menu);
         this.ui.container.appendChild(this.ui.content);
 
         this._eventListener();
-        this._requestGeneralPage();
+        this._requestShortcutsPage();
     }
 
 
@@ -72,7 +81,10 @@ class HelpCenterView extends View {
      * desc   : UserView event listeners
      **/
     _eventListener() {
-        this.ui.menuGen.addEventListener("click", this._requestGeneralPage.bind(this));
+        this.ui.menuShortcut.addEventListener("click", this._requestShortcutsPage.bind(this));
+        this.ui.menuTagConvention.addEventListener("click", this._requestTagConventionPage.bind(this));
+        this.ui.menuRanksPerms.addEventListener("click", this._requestRanksPermsPage.bind(this));
+        this.ui.menuManaCoin.addEventListener("click", this._requestManaCoinPage.bind(this));
     }
 
 
@@ -81,45 +93,58 @@ class HelpCenterView extends View {
      * class  : UserView
      * desc   : Display the general page
      **/
-    _requestGeneralPage() {
-        this._updateSettingsInfo();
+    _requestShortcutsPage() {
         this._clearPageSpace();
-        this.ui.menuGen.className          = "mzk-selected";
-        this.ui.contentTitle.innerHTML     = "General settings";
-
-        this.ui.rescanLibLabel            = document.createElement("P");
-        this.ui.rescanLibButton           = document.createElement("BUTTON");
-
-        this.ui.rescanLibLabel.innerHTML  = "<b>Remove my account</b><br>" +
-                                                "<br>" +
-                                                "Warning, this action will remove you from the database with all your data.<br>" +
-                                                "Are you sure you want to remove your account ?";
-        this.ui.rescanLibButton.innerHTML = "DELETE MY ACCOUNT";
-
-        this.ui.rescanLibButton.addEventListener("click", function() {
-            JSONParsedGetRequest(
-                "user/delete/",
-                function(response) {
-                    /* response = {
-                     *     DONE      : bool
-                     *     ERROR_H1  : string
-                     *     ERROR_MSG : string
-                     * } */
-                    if (response.DONE) {
-                        window.location.reload();
-                    }
-
-                    else {
-                        new Notification("ERROR", response.ERROR_H1, response.ERROR_MSG);
-                    }
-                }
-            );
-        });
+        this.ui.menuShortcut.className     = "mzk-selected";
+        this.ui.contentTitle.innerHTML     = "Shortcuts";
 
         this.ui.content.appendChild(this.ui.contentTitle);
         this.ui.content.appendChild(document.createElement("HR"));
-        this.ui.content.appendChild(this.ui.rescanLibLabel);
-        this.ui.content.appendChild(this.ui.rescanLibButton);
+    }
+
+
+    /**
+     * method : _requestGeneralPage (private)
+     * class  : UserView
+     * desc   : Display the general page
+     **/
+    _requestTagConventionPage() {
+        this._clearPageSpace();
+        this.ui.menuTagConvention.className = "mzk-selected";
+        this.ui.contentTitle.innerHTML      = "Tag convention";
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(document.createElement("HR"));
+    }
+
+
+    /**
+     * method : _requestGeneralPage (private)
+     * class  : UserView
+     * desc   : Display the general page
+     **/
+    _requestRanksPermsPage() {
+        this._clearPageSpace();
+        this.ui.menuRanksPerms.className = "mzk-selected";
+        this.ui.contentTitle.innerHTML      = "Ranks / Permissions";
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(document.createElement("HR"));
+    }
+
+
+    /**
+     * method : _requestGeneralPage (private)
+     * class  : UserView
+     * desc   : Display the general page
+     **/
+    _requestManaCoinPage() {
+        this._clearPageSpace();
+        this.ui.menuManaCoin.className = "mzk-selected";
+        this.ui.contentTitle.innerHTML      = "ManaCoin";
+
+        this.ui.content.appendChild(this.ui.contentTitle);
+        this.ui.content.appendChild(document.createElement("HR"));
     }
 
 
@@ -129,47 +154,10 @@ class HelpCenterView extends View {
      * desc   : Unselect every entry in the left menu
      **/
     _unselectAllMenuEntries() {
-        this.ui.menuGen.className = "";
-    }
-
-
-    /**
-     * method : _requestGeneralPage (private)
-     * class  : UserView
-     * desc   : Updates settings information
-     **/
-    _updateSettingsInfo(callback) {
-        let that = this;
-        JSONParsedGetRequest( // TODO : user the function in User class
-            "user/getSettings/",
-            function(response) {
-                /* response = {
-                 *     DONE      : bool
-                 *     ERROR_H1  : string
-                 *     ERROR_MSG : string
-                 *
-                 *     USERNAME:
-                 *     DATE_JOINED:
-                 *     LAST_LOGIN:
-                 *     INVITE_CODE:
-                 *     IS_ADMIN:
-                 *     MANACOIN:
-                 *     GODFATHER_CODE:
-                 *     GODFATHER_NAME:
-                 * } */
-                if (response.DONE) {
-                    that.info = response;
-
-                    if (callback) {
-                        callback();
-                    }
-                }
-
-                else {
-                    new Notification("ERROR", response.ERROR_H1, response.ERROR_MSG);
-                }
-            }
-        );
+        this.ui.menuShortcut.className = "";
+        this.ui.menuTagConvention.className = "";
+        this.ui.menuRanksPerms.className = "";
+        this.ui.menuManaCoin.className = "";
     }
 
 }
