c0373a142b5b4e92505ff593563010532e5ed934
Arthur Beaulieu <contact@abeaulieu.fr>
2018-06-10 23:10:52 +0200

    add english trads

--- static/js/Start.js
+++ static/js/Start.js
@@ -4,6 +4,25 @@ window.debug = true; // Debug variable : when activated, mzk will log every impo
 
 document.addEventListener('DOMContentLoaded', function() {
     window.app = new App(function() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                console.log(JSON.parse(this.responseText));
+            }
+        };
+
+        let a = JSON.stringify({
+            LANG: 'en'
+        });
+
+        console.log(a);
+
+        xhr.open('POST', '  language/', true);
+        xhr.setRequestHeader('X-CSRFToken', window.app.cookies['csrftoken']);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.send(a);
+
         window.app.init();
     });
 });
