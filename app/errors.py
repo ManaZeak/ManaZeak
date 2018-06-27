768b27040fad2e951c88eb2c6b5681a6f952deb9
Arthur Beaulieu <contact@abeaulieu.fr>
2018-06-27 18:23:07 +0200

    Hotfix python 2.

--- app/errors.py
+++ app/errors.py
@@ -90,103 +90,106 @@ def deactivateUser(user):
 # Generate the base of any status message
 # TODO: mettre caller a None
 def errorCheckMessage(isDone, error, caller, user=None):
-    errorKey = error.name
+    if error is None:
+        errorKey = None
+    else:
+        errorKey = error.name
 
-    if error == ErrorEnum.BAD_FORMAT:
-        if user is not None:
-            logger.error("Invalid json send to the function " + caller.__name__ + " by the user " + user.username)
-        else:
-            logger.error("Invalid request from a unlogged user at " + caller.__name__)
+        if error == ErrorEnum.BAD_FORMAT:
+            if user is not None:
+                logger.error("Invalid json send to the function " + caller.__name__ + " by the user " + user.username)
+            else:
+                logger.error("Invalid request from a unlogged user at " + caller.__name__)
 
-    elif error == ErrorEnum.BAD_REQUEST:
-        logger.warning("Invalid request type for the function " + caller.__name__)
+        elif error == ErrorEnum.BAD_REQUEST:
+            logger.warning("Invalid request type for the function " + caller.__name__)
 
-    elif error == ErrorEnum.DB_ERROR:
-        logger.error("A request returned an error in the function " + caller.__name__)
+        elif error == ErrorEnum.DB_ERROR:
+            logger.error("A request returned an error in the function " + caller.__name__)
 
-    elif error == ErrorEnum.FILE_NOT_FOUND:
-        logger.error("The file required wasn't found by the function " + caller.__name__)
+        elif error == ErrorEnum.FILE_NOT_FOUND:
+            logger.error("The file required wasn't found by the function " + caller.__name__)
 
-    elif error == ErrorEnum.DIR_NOT_FOUND:
-        logger.warning("The directory the user asked didn't exists called by " + caller.__name__)
+        elif error == ErrorEnum.DIR_NOT_FOUND:
+            logger.warning("The directory the user asked didn't exists called by " + caller.__name__)
 
-    elif error == ErrorEnum.EMPTY_LIBRARY:
-        logger.warning("The directory scanned by the user was empty called by " + caller.__name__)
+        elif error == ErrorEnum.EMPTY_LIBRARY:
+            logger.warning("The directory scanned by the user was empty called by " + caller.__name__)
 
-    elif error == ErrorEnum.COVER_ERROR:
-        logger.error("The function " + caller.__name__ + " can't create the file for the cover")
+        elif error == ErrorEnum.COVER_ERROR:
+            logger.error("The function " + caller.__name__ + " can't create the file for the cover")
 
-    elif error == ErrorEnum.PERMISSION_ERROR:
-        if user is not None:
-            logger.warning("The user " + user.username + " tried to do something not allowed")
-        else:
-            logger.error("The user parameter is missing for function " + caller.__name__)
+        elif error == ErrorEnum.PERMISSION_ERROR:
+            if user is not None:
+                logger.warning("The user " + user.username + " tried to do something not allowed")
+            else:
+                logger.error("The user parameter is missing for function " + caller.__name__)
 
-    elif error == ErrorEnum.RESCAN_ERROR:
-        logger.error("A rescan was already in progress called by " + caller.__name__)
+        elif error == ErrorEnum.RESCAN_ERROR:
+            logger.error("A rescan was already in progress called by " + caller.__name__)
 
-    elif error == ErrorEnum.NO_HISTORY:
-        if user is not None:
-            logger.info("The user " + user.username + " has finished his history")
-        else:
-            logger.error("The user parameter is missing for function " + caller.__name__)
+        elif error == ErrorEnum.NO_HISTORY:
+            if user is not None:
+                logger.info("The user " + user.username + " has finished his history")
+            else:
+                logger.error("The user parameter is missing for function " + caller.__name__)
 
-    elif error == ErrorEnum.NO_SAME_ARTIST:
-        logger.info("No same artist for the given track")
+        elif error == ErrorEnum.NO_SAME_ARTIST:
+            logger.info("No same artist for the given track")
 
-    elif error == ErrorEnum.NO_SAME_GENRE:
-        logger.info("No same genre for the given track")
+        elif error == ErrorEnum.NO_SAME_GENRE:
+            logger.info("No same genre for the given track")
 
-    elif error == ErrorEnum.NO_SAME_ALBUM:
-        logger.info("No same album for the given track")
+        elif error == ErrorEnum.NO_SAME_ALBUM:
+            logger.info("No same album for the given track")
 
-    elif error == ErrorEnum.SYNCTHING_ERROR:
-        logger.error("Syncthing couldn't be contacted")
+        elif error == ErrorEnum.SYNCTHING_ERROR:
+            logger.error("Syncthing couldn't be contacted")
 
-    elif error == ErrorEnum.USER_DELETE_ERROR:
-        if user is not None:
-            logger.critical("The user " + user.username + " tried to delete an admin user")
-        else:
-            logger.error("The user parameter is missing for function " + caller.__name__)
+        elif error == ErrorEnum.USER_DELETE_ERROR:
+            if user is not None:
+                logger.critical("The user " + user.username + " tried to delete an admin user")
+            else:
+                logger.error("The user parameter is missing for function " + caller.__name__)
 
-    elif error == ErrorEnum.NO_STATS:
-        logger.info("No stats were found")
+        elif error == ErrorEnum.NO_STATS:
+            logger.info("No stats were found")
 
-    elif error == ErrorEnum.BAD_FILE_NAME:
-        logger.error("The filename specified was correct call by " + caller.__name__)
+        elif error == ErrorEnum.BAD_FILE_NAME:
+            logger.error("The filename specified was correct call by " + caller.__name__)
 
-    elif error == ErrorEnum.FILE_EXISTS:
-        if user is not None:
-            logger.warning("The user " + user.username + " uploaded a song that already exists")
-        else:
-            logger.error("The user parameter is missing for function " + caller.__name__)
+        elif error == ErrorEnum.FILE_EXISTS:
+            if user is not None:
+                logger.warning("The user " + user.username + " uploaded a song that already exists")
+            else:
+                logger.error("The user parameter is missing for function " + caller.__name__)
 
-    elif error == ErrorEnum.VALUE_ERROR:
-        if user is not None:
-            logger.critical("The user " + user.username + " put a wrong type of value in a field. Called at "
-                            + caller.__name__)
-        else:
-            logger.error("The user parameter is missing for function " + caller.__name__)
+        elif error == ErrorEnum.VALUE_ERROR:
+            if user is not None:
+                logger.critical("The user " + user.username + " put a wrong type of value in a field. Called at "
+                                + caller.__name__)
+            else:
+                logger.error("The user parameter is missing for function " + caller.__name__)
 
-    elif error == ErrorEnum.DND_ERROR:
-        logger.critical("The configuration is not done correctly")
+        elif error == ErrorEnum.DND_ERROR:
+            logger.critical("The configuration is not done correctly")
 
-    elif error == ErrorEnum.FORMAT_ERROR:
-        logger.critical("The format of the given track has no support")
+        elif error == ErrorEnum.FORMAT_ERROR:
+            logger.critical("The format of the given track has no support")
 
-    elif error == ErrorEnum.DOWNLOAD_LIMIT:
-        logger.error("Too much track selected")
+        elif error == ErrorEnum.DOWNLOAD_LIMIT:
+            logger.error("Too much track selected")
 
-    elif error == ErrorEnum.DIR_CREATION_ERROR:
-        logger.error("The application can't create a folder for function " + caller.__name__)
+        elif error == ErrorEnum.DIR_CREATION_ERROR:
+            logger.error("The application can't create a folder for function " + caller.__name__)
 
-    elif error == ErrorEnum.SUSPICIOUS_OPERATION:
-        # If the user has an account
-        if user is not None:
-            logger.critical("The user : " + user.username + " tried to do some weird operation")
-            deactivateUser(user)
-        else:
-            logger.critical("An unauthenticated user tried to do some weird operation.")
+        elif error == ErrorEnum.SUSPICIOUS_OPERATION:
+            # If the user has an account
+            if user is not None:
+                logger.critical("The user : " + user.username + " tried to do some weird operation")
+                deactivateUser(user)
+            else:
+                logger.critical("An unauthenticated user tried to do some weird operation.")
 
     return {
         'DONE': isDone,
