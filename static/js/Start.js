a335a1bb0d40075d1386d941e96c347d53e9b595
Squadella <pierre.bouniole@gmail.com>
2018-07-13 09:53:48 +0200

    Removed all the javascript.

--- static/js/Start.js
+++
@@ -1,9 +0,0 @@
-import App from './App.js'
-
-window.debug = false; // Debug variable : when activated, mzk will log every important front-end call
-
-document.addEventListener('DOMContentLoaded', function() {
-    window.app = new App(() => {
-        window.app.init();
-    });
-});
