2e9262390e489f7be6b65b912825d2c4fc224d02
ArthurBeaulieu <contact@abeaulieu.fr>
2018-10-28 11:14:47 +0100

    UT in webpack
    Former-commit-id: 4f473f7eb0f1915e298dbc6c6a448c0606afc3f3

---
+++ doc/JavaScript/src/js/utils/ScrollBar.js
@@ -0,0 +1,164 @@
+'use_strict';
+
+class ScrollBar {
+  /**
+   * @summary Custom JavaScript ScrollBar for any conatiner
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Build a custom ScrollBar according to the given DOM target, inspired from https://github.com/buzinas/simple-scrollbar <3
+   * @param {object} options - The ScrollBar options
+   * @param {object} options.target - The DOM node to add a ScrollBar to
+   **/
+  constructor(options) {
+    this._target = options.target; // Parent div to put the ScrollBar in
+    this._wrapper = {}; // Wrap both container and ScrollBar
+    this._container = {}; // Content to scroll + browser ScrollBar (18px offset)
+    this._bar = {}; // ScrollBar itself
+    this._scrollRatio = 0;
+    this._lastPageY = 0;
+
+    this._init();
+    this._events();
+    this._updateScrollBar();
+  }
+
+  //  --------------------------------  PRIVATE METHODS  --------------------------------  //
+
+  /**
+   * @method
+   * @name _init
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Build DOM hierrarchy, ScrollBar double wraps the content to append its custom bar
+   **/
+  _init() {
+    const fragment = document.createDocumentFragment();
+    // Creating associated elements (wrapper, container, bar)
+    this._target.classList.add('scrollbar-container');
+    this._wrapper = document.createElement('DIV');
+    this._wrapper.setAttribute('class', 'scrollbar-wrapper');
+    this._container = document.createElement('DIV');
+    this._container.setAttribute('class', 'scrollbar-content');
+    // Move target children into this container
+    while (this._target.firstChild) {
+      this._container.appendChild(this._target.firstChild);
+    }
+    // Link DOM elements
+    this._wrapper.appendChild(this._container);
+    fragment.appendChild(this._wrapper);
+    // Append fragment to DOM target
+    this._target.appendChild(fragment);
+    this._target.insertAdjacentHTML('beforeend', '<div class="scroll"></div>'); // Append scroll as last child
+    this._bar = this._target.lastChild; // Get content from line just over this!
+    // Methods auto binding with this to be able to add/remove listeners easily
+    this._drag = this._drag.bind(this);
+    this._stopDrag = this._stopDrag.bind(this);
+  }
+
+  /**
+   * @method
+   * @name _events
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Handle ScrollBar mouse events
+   **/
+  _events() {
+    window.addEventListener('resize', this._updateScrollBar.bind(this));
+    this._container.addEventListener('scroll', this._updateScrollBar.bind(this));
+    this._container.addEventListener('mouseenter', this._updateScrollBar.bind(this));
+    this._bar.addEventListener('mousedown', this._barClicked.bind(this));
+  }
+
+  /**
+   * @method
+   * @name _drag
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Handle the drag animation of the bar
+   * @param {object} event - The Mouse event from this._events()
+   **/
+  _drag(event) {
+    const delta = event.pageY - this._lastPageY;
+    this._lastPageY = event.pageY;
+    requestAnimationFrame(() => {
+      this._container.scrollTop += (delta / this._scrollRatio);
+    });
+  }
+
+  /**
+   * @method
+   * @name _barClicked
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Add document events when bar is clicked to track the mouse movement in parent
+   * @param {object} event - The Mouse event from this._events()
+   **/
+  _barClicked(event) {
+    this._lastPageY = event.pageY;
+    this._bar.classList.add('scrollbar-grabbed');
+    document.body.classList.add('scrollbar-grabbed');
+    document.addEventListener('mousemove', this._drag);
+    document.addEventListener('mouseup', this._stopDrag);
+  }
+
+  /**
+   * @method
+   * @name _stopDrag
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Remove document events when bar is released
+   **/
+  _stopDrag() {
+    this._bar.classList.remove('scrollbar-grabbed');
+    document.body.classList.remove('scrollbar-grabbed');
+    document.removeEventListener('mousemove', this._drag);
+    document.removeEventListener('mouseup', this._stopDrag);
+  }
+
+  /**
+   * @method
+   * @name _updateScrollBar
+   * @private
+   * @memberof ScrollBar
+   * @author Arthur Beaulieu
+   * @since September 2018
+   * @description Compute bar position according to DOM measurements
+   **/
+  _updateScrollBar() {
+    const totalHeight = this._container.scrollHeight;
+    const ownHeight = this._container.clientHeight;
+    const right = (this._target.clientWidth - this._bar.clientWidth) * -1;
+
+    this._scrollRatio = ownHeight / totalHeight;
+    requestAnimationFrame(() => {
+      if (this._scrollRatio >= 1) { // Hide scrollbar if no scrolling is possible
+        this._bar.classList.add('hidden');
+      } else {
+        const height = (Math.max(this._scrollRatio * 100, 5) * ownHeight) / 100;
+        let top = ((this._container.scrollTop / totalHeight) * 100) * ownHeight / 100;
+
+        if (Math.max(this._scrollRatio * 100, 5) === 5) { // ScrollBar has reached its minimum size
+          /* Here is a complex thing : scroll total height != DOM node total height. We must substract
+          a growing percentage (as user goes down) that is scaled after total scroll progress in %. */
+          const scrollProgressPercentage = (this._container.scrollTop * 100) / (totalHeight - ownHeight);
+          top = ((ownHeight - height) * (((this._container.scrollTop + (scrollProgressPercentage * ownHeight) / 100) / totalHeight) * 100)) / 100;
+        }
+
+        this._bar.classList.remove('hidden');
+        this._bar.style.cssText = `height: ${height}px; top: ${top}px; right: ${right}px;`;
+      }
+    });
+  }
+}
+
+export default ScrollBar;
---
+++ doc/JavaScript/src/js/view/footbar/components/VolumeBar.js
@@ -0,0 +1,125 @@
+'use_strict';
+
+class VolumeBar {
+  /**
+   * @summary UI VolumeBar
+   * @author Arthur Beaulieu
+   * @since July 2018
+   * @description Interactive volume bar that is linked to ManaZeak logic
+   **/
+  constructor() {
+    this._volume = {
+      image: {},
+      wrapper: {},
+      container: {},
+      current: {},
+      thumb: {}
+    };
+    this._showHideTimeoutId = -1;
+    this.isDragging = false;
+
+    this._init();
+  }
+
+  //  --------------------------------  PRIVATE METHODS  --------------------------------  //
+
+  _init() {
+    this._volume = {
+      image: document.getElementById('volumebar-img'),
+      wrapper: document.getElementById('volumebar-wrapper'),
+      container: document.getElementById('volumebar-container'),
+      current: document.getElementById('volumebar-current'),
+      thumb: document.getElementById('volumebar-thumb')
+    };
+
+    this._events();
+  }
+
+  _events() {
+    this._volume.image.addEventListener('click', mzk.toggleMute.bind(mzk));
+    this._volume.container.addEventListener('mousedown', this._mouseDown.bind(this));
+
+    this._mouseMove = this._mouseMove.bind(this);
+    this._mouseUp = this._mouseUp.bind(this);
+  }
+
+  _mouseDown(event) {
+    if (!this.isDragging && (event.target.id === 'volumebar-wrapper' ||
+        event.target.id === 'volumebar-container' ||
+        event.target.id === 'volumebar-current' ||
+        event.target.id === 'volumebar-thumb')) {
+
+      this.isDragging = true;
+      this._moveVolume(event);
+
+      window.addEventListener('mousemove', this._mouseMove);
+      window.addEventListener('mouseup', this._mouseUp);
+    }
+  }
+
+  _mouseMove(event) {
+    if (this.isDragging) {
+      this._moveVolume(event);
+    }
+  }
+
+  _mouseUp() {
+    if (this.isDragging) {
+      this.isDragging = false;
+      window.removeEventListener('mousemove', this._mouseMove);
+      window.removeEventListener('mouseup', this._mouseUp);
+    }
+  }
+
+  _moveVolume(event) {
+    // Get container bound rectangle and compute difference in px and % (pr)
+    const boundRect = this._volume.container.getBoundingClientRect();
+    const toLeftInPx = event.clientX - boundRect.left; // Client X position minus container left X position equals X variation from container left side
+    let toLeftInPr = (toLeftInPx * 100) / boundRect.width; // Get width percentage depending on container width
+    // OOB protection
+    if (toLeftInPr > 100) {
+      toLeftInPr = 100;
+    }
+    if (toLeftInPr < 0) {
+      toLeftInPr = 0;
+    }
+    // Set mzk global volume
+    mzk.setVolume(toLeftInPr / 100);
+  }
+
+  //  --------------------------------  PUBLIC METHODS  ---------------------------------  //
+
+  updateVolume(isMuted, volume) {
+    const removeFullClass = () => {
+      if (this._volume.current.classList.contains('full')) {
+        this._volume.current.classList.remove('full');
+      }
+    };
+
+    volume *= 100;
+    // Icon update
+    if (volume === 0 || (typeof isMuted === 'boolean' && isMuted === true)) {
+      removeFullClass();
+      this._volume.image.src = '/static/img/player/volume-mute.svg';
+    } else if (volume > 0 && volume < 50) {
+      removeFullClass();
+      this._volume.image.src = '/static/img/player/volume-half.svg';
+    } else {
+      removeFullClass();
+      this._volume.image.src = '/static/img/player/volume-full.svg';
+    }
+
+    if (volume > 97 && volume <= 100) { // Add border radius on right side
+      this._volume.current.classList.add('full');
+    }
+
+    // Current and thumb update
+    if (typeof isMuted === 'boolean' && isMuted === true) {
+      volume = 0;
+    } // To set volume current and thumb at 0% left when muted.
+    this._volume.current.style.width = `${volume}%`;
+    this._volume.thumb.style.left = `${volume}%`;
+  }
+}
+
+export default VolumeBar;
