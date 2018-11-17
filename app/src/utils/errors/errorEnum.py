fe5ebddcb81ac869e105e8c6d63bafac0985fcfe
Squadella <pbouniol@et.esiea.fr>
2018-11-17 16:48:00 +0100

    Added permission and group system.

--- app/src/utils/errors/errorEnum.py
+++ app/src/utils/errors/errorEnum.py
@@ -5,73 +5,79 @@ from enum import unique, Enum
 ## The enumeration containing all the keys of each possible errors.
 class ErrorEnum(Enum):
     ## Bad json format send to the server.
-    BAD_FORMAT = 1
+    BAD_FORMAT = 'Bad json format send to the server.'
 
     ## Bad type of request send to the server.
-    BAD_REQUEST = 2
+    BAD_REQUEST = 'Bad request send to the server.'
 
     ## The db didn't return anything.
-    DB_ERROR = 3
+    DB_ERROR = 'The database didn\'t return anything.'
 
     ## The asked file didn't exist.
-    FILE_NOT_FOUND = 4
+    FILE_NOT_FOUND = 'The file asked doesn\'t exist.'
 
     ## The directory asked didn't exist.
-    DIR_NOT_FOUND = 5
+    DIR_NOT_FOUND = 'The directory asked doesn\'t exist.'
 
     ## The library asked is empty.
-    EMPTY_LIBRARY = 6
+    EMPTY_LIBRARY = 'The library asked is empty.'
 
     ## The directory for covers cannot be created.
-    COVER_ERROR = 7
+    COVER_ERROR = 'The covers cannot be created.'
 
     ## The user didn't have enough permission.
-    PERMISSION_ERROR = 8
+    PERMISSION_ERROR = 'The user doesn\'t have the required permission.'
 
     ## Error during the rescan.
-    RESCAN_ERROR = 9
+    RESCAN_ERROR = 'The rescan encountered an error.'
 
     ## No track history for the given user.
-    NO_HISTORY = 10
+    NO_HISTORY = 'The user has no track history.'
 
     ## No same artist for the given track.
-    NO_SAME_ARTIST = 11
+    NO_SAME_ARTIST = 'No other tracks of the artist has been found in the database.'
 
     ## No same genre for the given track.
-    NO_SAME_GENRE = 12
+    NO_SAME_GENRE = 'No other tracks of the same genre has been found in the database.'
 
     ## No same album for the given track.
-    NO_SAME_ALBUM = 13
-
-    ## Syncthing encountered an error.
-    SYNCTHING_ERROR = 14
+    NO_SAME_ALBUM = 'No other track of the same album has been found on the server.'
 
     ## Some user tried to delete an admin user.
-    USER_DELETE_ERROR = 15
+    USER_DELETE_ERROR = 'You tried to delete an admin user.'
 
     ## No stats for the given user.
-    NO_STATS = 16
+    NO_STATS = 'No stats for the given user'
 
     ## Bad filename for the upload.
-    BAD_FILENAME = 17
+    BAD_FILENAME = 'The filename isn\'t correct.'
 
     ## The file didn't exists.
-    FILE_EXISTS = 18
+    FILE_EXISTS = 'The file doesn\'t exists.'
 
     ## Wrong format of input inside the JSON response.
-    VALUE_ERROR = 19
+    VALUE_ERROR = 'A unexpected type was found inside the json.'
 
     ## Error during the file uplaod.
-    DND_ERROR = 20
+    DND_ERROR = 'Error during the file upload'
 
     ## Wrong format of audio file.
-    FORMAT_ERROR = 21
+    FORMAT_ERROR = 'Wrong audio format'
 
     ## Limit for download has been reached.
-    DOWNLOAD_LIMIT = 22
+    DOWNLOAD_LIMIT = 'The user reached the download limit.'
 
     ## Cannot create a directory.
-    DIR_CREATION_ERROR = 23
+    DIR_CREATION_ERROR = 'The directory cannot be created.'
+
+    ## Cannot find the user invite code
+    INVITE_CODE_NOT_FOUND = 'The invite code provided doesn\'t exists.'
+
+    ## Cannot find the user settings
+    UNKNOWN_USER_SETTINGS = 'The user given has no settings.'
+
+    ## The application is in a unexpected state
+    UNEXPECTED_STATE = 'The application is in a unexpected state.'
 
     ## Suspicious operation for a user.
-    SUSPICIOUS_OPERATION = 24
+    SUSPICIOUS_OPERATION = 'The user did a suspicious operation (hacking?)'
--- app/src/utils/userSettingsManager.py
+++ app/src/utils/userSettingsManager.py
@@ -1,25 +1,41 @@
+from app.models import ApplicationConfiguration
 from app.models.security import Group
 from app.models.settings import UserSetting
+from app.src.utils.applicationConfigurationManager import ApplicationConfigurationManager
 from app.src.utils.avatars.avatarGenerator import AvatarGenerator
 
 
-## @package app.utils.userSettingsManager
-# Control the users settings
+# Controls the users settings
+from app.src.utils.errors.errorEnum import ErrorEnum
+from app.src.utils.exceptions.userException import UserException
 
-## Create the user settings in database for a user
-#   @param user the user to be linked with the settings created
-#   @param inviteCode the invite code the user used if any
-def createUserSettings(user, inviteCode):
-    userPref = UserSetting()
-    userPref.user = user
-    # Special group if the user is an admin
-    # FIXME : changer avec un sytème qui fonctionne si on créer de nouveau groupe
-    if user.is_superuser:
-        userPref.group = Group.objects.get(rank=5)
-    else:
-        userPref.group = Group.objects.get(rank=1)
-    # Setting the user avatar
-    userPref.avatar = AvatarGenerator().generateAvatar(user.username)
-    if inviteCode is not None:
-        userPref.inviteCode = inviteCode
-    userPref.save()
+
+class UserSettingsManager:
+
+    @staticmethod
+    ## Create the user settings in database for a user
+    #   @param user the user to be linked with the settings created
+    #   @param inviteCode the invite code the user used if any
+    def createUserSettings(user, inviteCode):
+        userPref = UserSetting()
+        userPref.user = user
+        # Special group if the user is an admin
+        if user.is_superuser:
+            userPref.group = Group.objects.get(rank=5)
+        else:
+            # Use the default group of the application
+            userPref.group = ApplicationConfigurationManager.getApplicationConfiguration().defaultGroup
+        # Setting the user avatar
+        userPref.avatar = AvatarGenerator().generateAvatar(user.username)
+        if inviteCode is not None:
+            userPref.inviteCode = inviteCode
+        userPref.save()
+
+    @staticmethod
+    ## Get the user settings from the database
+    def getUserSettings(user):
+        userSettings = UserSetting.objects.filter(user=user)
+        if userSettings is not None:
+            return UserSetting.objects.get(user=user)
+        else:
+            raise UserException(ErrorEnum.UNKNOWN_USER_SETTINGS)
