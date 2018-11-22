a75bc186af1d7d921959e590547847f0232d8fca
ArthurBeaulieu <contact@abeaulieu.fr>
2018-11-22 18:52:48 +0100

    Code smell reduction anotha four

--- src/js/view/maincontainer/SceneView.js
+++ src/js/view/maincontainer/SceneView.js
@@ -24,6 +24,62 @@ class SceneView {
     }
   }
 
+  _simpleClick(targetId) {
+    const isTargetSelected = this._tracks[targetId].getIsSelected(); // Saving target selection state before unselecting all
+    this.unselectAll();
+
+    if (isTargetSelected) {
+      this._tracks[targetId].setSelected(false); // Restore previous state to properly use in Normal click behavior condition
+      this._selection.splice(this._selection.indexOf(targetId), 1);
+    }
+  }
+
+  _doubleClick(targetId) {
+    clearTimeout(this._click.timeoutId);
+    this.stopPlayback();
+    this._tracks[targetId].setSelected(true);
+    this._selection.push(parseInt(targetId, 10));
+    mzk.changeTrack(this._tracks[targetId].id);
+  }
+
+  _toggleSelected(targetId) {
+    mzk.view.startLoading()
+      .then(() => {
+        if (this._tracks[targetId].getIsSelected()) {
+          this._tracks[targetId].setSelected(false);
+          this._selection.splice(this._selection.indexOf(targetId), 1);
+        } else {
+          this._tracks[targetId].setSelected(true);
+          this._selection.push(parseInt(targetId, 10));
+        }
+
+        mzk.view.stopLoading();
+      });
+  }
+
+  _controlShiftClick(targetId) {
+    mzk.view.startLoading()
+      .then(() => {
+        let start = 0;
+        let end = 0;
+
+        if (parseInt(targetId, 10) < this._selection[0]) { // Compare to this._selection[0] since this._selection is always ordered
+          start = parseInt(targetId, 10);
+          end = this._selection[0];
+        } else if (parseInt(targetId, 10) > this._selection[this._selection.length - 1]) { // Same here with greater index in this._selection
+          start = this._selection[this._selection.length - 1] + 1; // +1  to avoid first item repetition
+          end = parseInt(targetId, 10) + 1; // +1 to not forget the targetId too
+        }
+
+        for (let i = start; i < end; ++i) { // Loop to fill in between items
+          this._tracks[i].setSelected(true);
+          this._selection.push(i);
+        }
+
+        mzk.view.stopLoading();
+      });
+  }
+
   _trackClicked(event) {
     event.stopPropagation(); // Block window click listener
 
@@ -38,65 +94,24 @@ class SceneView {
     }
 
     if (!this._click.dbclick || this._click.targetId !== targetId) { // Second test force dbclick to occur on same track
-      this._click.dbclick = true;
+      this._click.dbclick = true; // Activate double click lock
       this._click.targetId = targetId;
 
       if (!event.ctrlKey) { // Simple click unselects all, return to avoid side effects. Unselect then done
-        const isTargetSelected = this._tracks[targetId].getIsSelected(); // Saving target selection state before unselecting all
-        this.unselectAll();
-
-        if (isTargetSelected) {
-          this._tracks[targetId].setSelected(false); // Restore previous state to properly use in Normal click behavior condition
-          this._selection.splice(this._selection.indexOf(targetId), 1);
-          return;
-        }
+        this._simpleClick(targetId);
       }
 
       if (event.ctrlKey && event.shiftKey && this._selection.length > 0) { // Ctrl + Shift + Click : fill selection in between target and closest selectioned track
-        mzk.view.startLoading()
-          .then(() => {
-            let start = 0;
-            let end = 0;
-
-            if (parseInt(targetId, 10) < this._selection[0]) { // Compare to this._selection[0] since this._selection is always ordered
-              start = parseInt(targetId, 10);
-              end = this._selection[0];
-            } else if (parseInt(targetId, 10) > this._selection[this._selection.length - 1]) { // Same here with greater index in this._selection
-              start = this._selection[this._selection.length - 1] + 1; // +1  to avoid first item repetition
-              end = parseInt(targetId, 10) + 1; // +1 to not forget the targetId too
-            }
-
-            for (let i = start; i < end; ++i) { // Loop to fill in between items
-              this._tracks[i].setSelected(true);
-              this._selection.push(i);
-            }
-
-            mzk.view.stopLoading();
-          });
-      } else { // Normal click behavior
-        mzk.view.startLoading()
-          .then(() => {
-            if (this._tracks[targetId].getIsSelected()) {
-              this._tracks[targetId].setSelected(false);
-              this._selection.splice(this._selection.indexOf(targetId), 1);
-            } else {
-              this._tracks[targetId].setSelected(true);
-              this._selection.push(parseInt(targetId, 10));
-            }
-
-            mzk.view.stopLoading();
-          });
+        this._controlShiftClick(targetId);
+      } else { // Ctrl click behavior (+ normal click behavior toggle selected)
+        this._toggleSelected(targetId);
       }
 
       this._click.timeoutId = setTimeout(() => {
-        this._click.dbclick = false;
-      }, 300); // Double click speed lower than 300ms
-    } else {
-      clearTimeout(this._click.timeoutId);
-      this.stopPlayback();
-      this._tracks[targetId].setSelected(true);
-      this._selection.push(parseInt(targetId, 10));
-      mzk.changeTrack(this._tracks[targetId].id);
+        this._click.dbclick = false; // Desactivate double click lock
+      }, 300);
+    } else { // Dble click
+      this._doubleClick(targetId);
     }
 
     this._selection.sort((a, b) => {
