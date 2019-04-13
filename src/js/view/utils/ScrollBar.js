4d6b1e66b46f2bf9eeeccce754b6f00304b778e8
ArthurBeaulieu <contact@abeaulieu.fr>
2019-04-13 20:26:02 +0200

    fix scrollbar grabbed bug

--- src/js/view/utils/ScrollBar.js
+++ src/js/view/utils/ScrollBar.js
@@ -103,6 +103,7 @@ class ScrollBar {
    **/
   _barClicked(event) {
     this._lastPageY = event.pageY;
+    this._wrapper.firstChild.style.scrollBehavior = 'inherit';
     this._bar.classList.add('scrollbar-grabbed');
     document.body.classList.add('scrollbar-grabbed');
     document.addEventListener('mousemove', this._drag);
@@ -119,6 +120,7 @@ class ScrollBar {
    * @description Remove document events when bar is released
    **/
   _stopDrag() {
+    this._wrapper.firstChild.style.scrollBehavior = 'smooth';
     this._bar.classList.remove('scrollbar-grabbed');
     document.body.classList.remove('scrollbar-grabbed');
     document.removeEventListener('mousemove', this._drag);
