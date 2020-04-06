7dd85cc63ff8582fb6c018138f62e1405f3b0137
Squadella <pbouniol@et.esiea.fr>
2020-04-06 21:10:39 +0200

    Added URL to manually launch the thumbnail regeneration. The thread of the thumbnail generator is launched properly.

--- app/src/services/thumbs/thumbnailService.py
+++ app/src/services/thumbs/thumbnailService.py
@@ -1,10 +1,16 @@
 from multiprocessing import Process
 
 from django import db
+from django.contrib.auth.decorators import login_required
 
+from app.src.security.permissionEnum import PermissionEnum
+from app.src.security.permissionHandler import PermissionHandler
 from app.src.services.thumbs.artistThumbnailService import ArtistThumbnailService
 from app.src.services.thumbs.coverThumbnailService import CoverThumbnailService
 from app.src.services.thumbs.labelThumbnailService import LabelThumbnailService
+from app.src.utils.decorators.frontRequest import FrontRequest
+from app.src.utils.frontRequestChecker import FrontRequestChecker
+from app.src.utils.requestMethodEnum import RequestMethodEnum
 
 
 ## This service manages all the thumbnail generation.
@@ -14,7 +20,7 @@ class ThumbnailService(object):
     ## Start the process that will generate the thumbnails of the library.
     def startProcessRegenThumbnails():
         scanThread = Process(
-            target=ThumbnailService.regenerateAllThumbnails()
+            target=ThumbnailService.regenerateAllThumbnails
         )
         # Closing all connection to the database for avoiding to use the same connection between processes.
         db.connections.close_all()
@@ -33,3 +39,16 @@ class ThumbnailService(object):
         # Launch the cover thumbnail generation
         coverThumbnailService = CoverThumbnailService()
         coverThumbnailService.regenerateThumbnailForAllDbCovers()
+
+    @staticmethod
+    @login_required(redirect_field_name='', login_url='app:login')
+    @FrontRequest
+    def regeneratesAllThumbnailsAdmin(request):
+        # Getting the user.
+        user = request.user
+        # Checking the permission of the user
+        PermissionHandler.checkPermission(PermissionEnum.ADMIN_VIEW, user)
+        # Check the request of the user.
+        FrontRequestChecker.checkRequest(RequestMethodEnum.GET, request, user)
+        # Launch the thread for regenerating thumbnails.
+        ThumbnailService.startProcessRegenThumbnails()
--- app/urls/admin/adminUrls.py
+++ app/urls/admin/adminUrls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from app.src.services.admin.adminService import AdminService
 from app.src.services.admin.adminTools import AdminTools
 from app.src.services.config.configService import ConfigService
+from app.src.services.thumbs.thumbnailService import ThumbnailService
 
 app_name = 'app'
 
@@ -10,6 +11,9 @@ app_name = 'app'
 urlpatterns = [
     path('reset/', AdminTools.resetInstance, name='reset'),
     path('toggleInvite/', AdminService.toggleInviteMode, name='toggleInvite'),
+
+    path('regenerateThumbnails/', ThumbnailService.regeneratesAllThumbnailsAdmin, name='regenerateThumbs'),
+
     # FIXME: to be moved into confs
     path('isInviteEnabled/', ConfigService.isInviteCodeEnabled, name='inviteEnabled'),
     # Page for logging the performance of the application.
