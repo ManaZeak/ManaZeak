90377647c4645216ce8380132e963e948abfc9ca
Squadella <pbouniol@et.esiea.fr>
2020-04-09 20:42:47 +0200

    Added a URL for getting all the users of the applications.

--- app/urls/admin/adminUrls.py
+++ app/urls/admin/adminUrls.py
@@ -4,6 +4,7 @@ from app.src.services.admin.adminService import AdminService
 from app.src.services.admin.adminTools import AdminTools
 from app.src.services.config.configService import ConfigService
 from app.src.services.thumbs.thumbnailService import ThumbnailService
+from app.src.services.user.userService import UserService
 
 app_name = 'app'
 
@@ -13,6 +14,8 @@ urlpatterns = [
     path('toggleInvite/', AdminService.toggleInviteMode, name='toggleInvite'),
 
     path('regenerateThumbnails/', ThumbnailService.regeneratesAllThumbnailsAdmin, name='regenerateThumbs'),
+
+    path('getAllUsers/', UserService.getAllUsers, name='getAllUsers'),
 
     # FIXME: to be moved into confs
     path('isInviteEnabled/', ConfigService.isInviteCodeEnabled, name='inviteEnabled'),
