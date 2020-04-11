c2c5600d1a3accfbde57f9a4a4f93c17e8be1573
Squadella <pbouniol@et.esiea.fr>
2020-04-11 20:04:22 +0200

    Added tag in the HTML.

--- app/templatetags/mzkWorldMapTag.py
+++ app/templatetags/mzkWorldMapTag.py
@@ -6,14 +6,14 @@ register = template.Library()
 
 @register.simple_tag(name='mzkWorldMapIncludeCss')
 def mzkWorldMapPluginIncludeCss():
-    if os.path.isfile('../../static/dist/css/mzkworldmap.bundle.css'):
+    if os.path.isfile('/static/dist/css/mzkworldmap.bundle.css'):
         return format_html(
             '<link rel="stylesheet" type="text/css" href="/static/dist/css/mzkworldmap.bundle.css"/>')
     return ''
 
 @register.simple_tag(name='mzkWorldMapIncludeJs')
 def mzkWorldMapPluginIncludeJs():
-    if os.path.isfile('../../static/dist/js/mzkworldmap.bundle.js'):
+    if os.path.isfile('/static/dist/js/mzkworldmap.bundle.js'):
         return format_html(
             '<script type="text/javascript" src="/static/dist/js/mzkworldmap.bundle.js" defer/></script>')
     return ''
