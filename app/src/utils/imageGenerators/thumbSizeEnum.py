cf874ff04d6248239bab636062b7f8f86c825f0b
ArthurBeaulieu <contact@abeaulieu.fr>
2020-05-07 19:04:14 +0200

    update thumb sizes in enum

--- app/src/utils/imageGenerators/thumbSizeEnum.py
+++ app/src/utils/imageGenerators/thumbSizeEnum.py
@@ -5,10 +5,10 @@ from enum import Enum, unique
 ## This enum describes the different formats of the thumbnails.
 class ThumbSizeEnum(Enum):
     ## A tiny thumbnail (80x80). Used on the track preview for example.
-    TINY = (80, 80)
-    ## A small thumbnail (120x120). Used on the main page for example.
-    SMALL = (120, 120)
+    TINY = (100, 100)
+    ## A small thumbnail (140x140). Used on the main page for example.
+    SMALL = (140, 140)
     ## A normal thumbnail (285x285). Used on the detail sceneviews.
-    MEDIUM = (285, 285)
+    MEDIUM = (300, 300)
     ## A large thumbnail (400x400). Used on the detail sceneviews.
-    LARGE = (400, 400)
+    LARGE = (425, 425)
