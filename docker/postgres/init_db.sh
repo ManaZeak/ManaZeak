ad4ddf8bd5ec89e08323873c3148c788d8654229
Squadella <pierre.bouniole@gmail.com>
2020-06-15 23:33:09 +0200

    Docker deployment now works properly.

--- docker/postgres/init_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE music;
-    GRANT ALL PRIVILEGES ON DATABASE music TO postgres;
-EOSQL
