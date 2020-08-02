767d071e2e4c8336e234bb0fe2e42ba1abdfacff
Squadella <pierre.bouniole@gmail.com>
2020-08-02 11:20:09 +0200

    Adding a unit test for additional information of the user.

--- back/src/test/java/org/manazeak/manazeak/service/security/user/UserAddInformationTest.java
+++ back/src/test/java/org/manazeak/manazeak/service/security/user/UserAddInformationTest.java
@@ -1,6 +1,7 @@
 package org.manazeak.manazeak.service.security.user;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.manazeak.manazeak.AbstractManaZeakTest;
 import org.manazeak.manazeak.datacreation.security.user.MzkUserDataCreation;
 import org.manazeak.manazeak.datacreation.security.user.UserFirstInfoDataCreation;
@@ -9,6 +10,7 @@ import org.manazeak.manazeak.entity.dto.user.UserFirstInfoDto;
 import org.manazeak.manazeak.entity.security.MzkUser;
 import org.manazeak.manazeak.service.file.TestImageGetter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -16,7 +18,7 @@ import java.util.Optional;
 /**
  * This class test that the add user information works properly.
  */
-public class UserAddInformationTest extends AbstractManaZeakTest {
+class UserAddInformationTest extends AbstractManaZeakTest {
 
     @Autowired
     private MzkUserDataCreation userDataCreation;
@@ -32,6 +34,9 @@ public class UserAddInformationTest extends AbstractManaZeakTest {
     /**
      * Test that when a user add some information to his profile it fills the database.
      */
+
+    @Test
+    @WithMockUser(username = UserTestConstants.USERNAME)
     void testAddUserInformation() throws IOException {
         // Creating the default user.
         userDataCreation.createDefaultMzkUser();
