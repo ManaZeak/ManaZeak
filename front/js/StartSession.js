532ff1127203d2d319cb427e3133c15fca38f0c9
Arthur Beaulieu <contact@abeaulieu.fr>
2020-08-25 00:53:21 +0200

    user profile init

--- front/js/StartSession.js
+++ front/js/StartSession.js
@@ -2,4 +2,11 @@ import Kom from './core/Kom.js';
 'use strict';
 
 console.log('Successfully loaded JS bundle');
-new Kom();
+const kom = new Kom();
+/* WIP */
+const userTmp = document.getElementByClassName('user-avatar')[0];
+userTmp.onClick = () => {
+    kom.getText('/fragments/user/userProfile').then(response => {
+        console.log(response);
+    });
+};
