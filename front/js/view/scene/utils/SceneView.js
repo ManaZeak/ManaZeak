a5d556d6b7af119e937bff1cf4cdef01f9794948
Arthur Beaulieu <contact@abeaulieu.fr>
2020-09-27 21:51:57 +0200

    doc again

--- front/js/view/scene/utils/SceneView.js
+++ front/js/view/scene/utils/SceneView.js
@@ -11,10 +11,18 @@ class SceneView {
    * are based on an HTML template, that will be loaded the parsed to be included in the DOM scene. When the view
    * building is done, a <code>SceneViewReady</code> event is fired through the custom event proxy.</blockquote> */
   constructor() {
+    /** @public
+     * @member {object} - The view wrapper div */
     this.wrapper = null;
   }
 
 
+  /** @method
+   * @name destroy
+   * @public
+   * @memberof SceneView
+   * @description <blockquote>The destroy method will clear the wrapper. A destroy method must be created in child
+   * class to properly clean itself. It should also call for <code>super</code> to call this method.</blockquote> */
   destroy() {
     this.wrapper = null;
   }
@@ -27,6 +35,14 @@ class SceneView {
   /*  --------------------------------------------------------------------------------------------------------------- */
 
 
+  /** @method
+   * @name _fetchWrapper
+   * @private
+   * @memberof UserInterface
+   * @description <blockquote>This method will request the HTML template for the given url. It will then parse it
+   * and update the view wrapper to match this newly loaded template.</blockquote>
+   * @param {string} url - The template url to load html from
+   * @return {promise} - The action promise */
   _fetchWrapper(url) {
     return new Promise((resolve, reject) => {
       mzk.kom.getText(url)
@@ -39,6 +55,13 @@ class SceneView {
   }
 
 
+  /** @method
+   * @name _viewReady
+   * @private
+   * @memberof UserInterface
+   * @description <blockquote>This method needs to be called last, when all the view initialisation is done. This
+   * way, it will notify the UserInterface controller that te view creation is done, and that it should release the
+   * UI removing the loading overlay.</blockquote> */
   _viewReady() {
     Events.publish('SceneViewReady');
   }
@@ -49,11 +72,15 @@ class SceneView {
   /*  --------------------------------------------------------------------------------------------------------------- */
 
 
+  /** @public
+   * @member {object} - The view first DOM child in template */
   get dom() {
     return this.wrapper;
   }
 
 
+  /** @public
+   * @member {object} - The view first DOM child in template */
   set dom(dom) {
     this.wrapper = dom;
   }
