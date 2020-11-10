dade8634679004e959f8d67cbf20885d45a0a698
Squadella <pierre.bouniole@gmail.com>
2020-11-10 21:34:56 +0100

    Finished the wish creation with the modal.

--- back/src/main/java/org/manazeak/manazeak/controller/html/fragment/user/wish/WishCreationFragment.java
+++ back/src/main/java/org/manazeak/manazeak/controller/html/fragment/user/wish/WishCreationFragment.java
@@ -3,13 +3,15 @@ package org.manazeak.manazeak.controller.html.fragment.user.wish;
 import org.manazeak.manazeak.configuration.security.rest.RestSecurity;
 import org.manazeak.manazeak.constant.security.PrivilegeEnum;
 import org.manazeak.manazeak.controller.html.fragment.FragmentController;
+import org.manazeak.manazeak.controller.page.GeneralFragmentEnum;
 import org.manazeak.manazeak.controller.page.user.wish.WishFragmentEnum;
-import org.manazeak.manazeak.entity.dto.KommunicatorObject;
 import org.manazeak.manazeak.entity.dto.user.wish.UserWishDto;
+import org.manazeak.manazeak.service.security.user.wish.WishService;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
@@ -20,11 +22,21 @@ import javax.validation.Valid;
 public class WishCreationFragment {
 
     /**
+     * The service for manipulating wishes
+     */
+    private final WishService wishService;
+
+    public WishCreationFragment(WishService wishService) {
+        this.wishService = wishService;
+    }
+
+    /**
      * Get the modal for creating the user information.
      *
      * @param model the data contained inside the page.
      * @return the fragment for creating the wish.
      */
+    @RestSecurity(PrivilegeEnum.WISH)
     @GetMapping("/wish")
     public String getModalWishUserCreation(Model model) {
         model.addAttribute("wish", new UserWishDto());
@@ -33,25 +45,19 @@ public class WishCreationFragment {
 
     /**
      * Save the user wishes into the database if the input is correct.
-     * @return the page.
-     */
-    public String saveUserWish() {
-
-    }
-
-    /**
-     * Create a wish for the current user.
      *
-     * @return The status of the request.
+     * @return the page.
      */
     @RestSecurity(PrivilegeEnum.WISH)
     @PostMapping("/wish")
-    public KommunicatorObject createWishForCurrentUser(@RequestParam @Valid UserWishDto wishDto) {
-        wishService.saveCurrentUserWish(wishDto);
-        return new KommunicatorObject();
-    }
-
-    private Model getWishCreationPage() {
-
+    public String saveUserWish(@ModelAttribute("wish") @Valid UserWishDto wish, BindingResult result) {
+        // Returning into the modal of wish creation.
+        if (result.hasErrors()) {
+            return WishFragmentEnum.WISH_CREATION.getPage();
+        }
+        // Creating a wish for the user.
+        wishService.saveCurrentUserWish(wish);
+        // Returns an empty fragment in case of success.
+        return GeneralFragmentEnum.EMPTY_PAGE.getPage();
     }
 }
---
+++ back/src/main/java/org/manazeak/manazeak/controller/page/GeneralFragmentEnum.java
@@ -0,0 +1,18 @@
+package org.manazeak.manazeak.controller.page;
+
+/**
+ * Contains the common pages for the fragments.
+ */
+public enum GeneralFragmentEnum {
+    EMPTY_PAGE("fragments/empty-response.html");
+
+    private final String page;
+
+    GeneralFragmentEnum(String page) {
+        this.page = page;
+    }
+
+    public String getPage() {
+        return page;
+    }
+}
