3e22783557a7e2ea841e42826bc7d6bcb686009e
Squadella <pierre.bouniole@gmail.com>
2020-11-27 09:13:09 +0100

    Modified the empty return with a JSON return.

--- back/src/main/java/org/manazeak/manazeak/controller/page/GeneralFragmentEnum.java
+++ back/src/main/java/org/manazeak/manazeak/controller/page/GeneralFragmentEnum.java
@@ -4,7 +4,7 @@ package org.manazeak.manazeak.controller.page;
  * Contains the common pages for the fragments.
  */
 public enum GeneralFragmentEnum {
-    EMPTY_PAGE("fragments/empty-response.html");
+    EMPTY_PAGE("fragments/success-response.html");
 
     private final String page;
 
