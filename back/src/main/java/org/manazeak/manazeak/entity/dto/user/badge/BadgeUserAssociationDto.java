b2dc5ca625120205dffbc5339ff3f47e4d40c8d9
Arthur Beaulieu <contact@abeaulieu.fr>
2020-12-03 23:36:31 +0100

    fixme associate badge

--- back/src/main/java/org/manazeak/manazeak/entity/dto/user/badge/BadgeUserAssociationDto.java
+++ back/src/main/java/org/manazeak/manazeak/entity/dto/user/badge/BadgeUserAssociationDto.java
@@ -8,11 +8,16 @@ import javax.validation.constraints.NotEmpty;
  * Allows to associate a user and a badge
  */
 public class BadgeUserAssociationDto {
+    // FIXME not null instead of not empty
+    //@NotEmpty(message = "{user.badge.empty_id}")
+    private Long userId;
 
-    @NotEmpty(message = "{user.badge.empty_id}")
-    private final Long userId;
-    @NotEmpty(message = "{user.empty_id}")
-    private final Long badgeId;
+    //@NotEmpty(message = "{user.empty_id}")
+    private Long badgeId;
+
+    public BadgeUserAssociationDto() {
+
+    }
 
     public BadgeUserAssociationDto(final Long userId, final Long badgeId) {
         this.userId = userId;
@@ -26,4 +31,13 @@ public class BadgeUserAssociationDto {
     public Long getBadgeId() {
         return badgeId;
     }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public void setBadgeId(Long badgeId) {
+        this.badgeId = badgeId;
+    }
+
 }
