360aeeb430edf38438be0fd98deb8d38b72beb8d
Arthur Beaulieu <contact@abeaulieu.fr>
2020-12-04 20:05:27 +0100

    mobile support for drag and drop

--- front/js/utils/DragElement.js
+++ front/js/utils/DragElement.js
@@ -4,25 +4,89 @@ class DragElement {
   constructor(options) {
     this._target = options.target;
     this._data = options.data;
+
     this._dragStartEvtId = -1;
-    this._target.setAttribute('draggable', 'true');
+
+    this._touchStartEvtId = -1;
+    this._touchMoveEvtId = -1;
+    this._touchEndEvtId = -1;
+    this._touchStarted = false;
+    this._touchPhantomDom = null;
+
+    this._buildElements();
     this._events();
   }
 
 
   destroy() {
     Events.removeEvent(this._dragStartEvtId);
+    Events.removeEvent(this._touchStartEvtId);
+    Events.removeEvent(this._touchMoveEvtId);
+    Events.removeEvent(this._touchEndEvtId);
     Utils.removeAllObjectKeys(this);
+  }
+
+
+  _buildElements() {
+    this._touchPhantomDom = this._target.cloneNode(true);
+    this._touchPhantomDom.style.position = 'fixed';
+    this._touchPhantomDom.style.opacity = '.5';
+    this._target.setAttribute('draggable', 'true');
   }
 
 
   _events() {
     this._dragStartEvtId = Events.addEvent('dragstart', this._target, this._dragStart, this);
+    this._touchStartEvtId = Events.addEvent('touchstart', this._target, this._dragStart, this);
+    this._touchMoveEvtId = Events.addEvent('touchmove', this._target, this._dragTouchMove, this);
+    this._touchEndEvtId = Events.addEvent('touchend', this._target, this._dragTouchEnd, this);
   }
 
 
   _dragStart(event) {
-    event.dataTransfer.setData('text/plain', JSON.stringify(this._data));
+    if (event.dataTransfer) { // Desktop behavior
+      event.dataTransfer.setData('text/plain', JSON.stringify(this._data));
+    } else { // Mobile behavior
+      event.preventDefault(); // Avoid context to be triggered on touch kept pushed
+      this._touchStarted = true;
+      // Define style for phantom DIV according to the dragged item style values
+      const rect = this._target.getBoundingClientRect();
+      const style = window.getComputedStyle(this._target); // Get margin, as they make the position calculus wrong
+      const leftMargin = parseInt(style.marginLeft.replace('px', ''));
+      const topMargin = parseInt(style.marginTop.replace('px', ''));
+      this._touchPhantomDom.style.top = `${rect.top - topMargin}px`;
+      this._touchPhantomDom.style.left = `${rect.left - leftMargin}px`;
+      this._touchPhantomDom.style.height = `${rect.height}px`;
+      this._touchPhantomDom.style.width = `${rect.width}px`;
+      this._touchPhantomDom.dataset.startX = event.targetTouches[0].pageX - rect.left + leftMargin;
+      this._touchPhantomDom.dataset.startY = event.targetTouches[0].pageY - rect.top + topMargin;
+      // Append to DOM parent to have the exact same style
+      this._target.parentNode.appendChild(this._touchPhantomDom);
+    }
+  }
+
+
+  _dragTouchMove(event) {
+    if (this._touchStarted === true) {
+      this._touchPhantomDom.style.top = `${event.targetTouches[0].pageY - this._touchPhantomDom.dataset.startY}px`;
+      this._touchPhantomDom.style.left = `${event.targetTouches[0].pageX - this._touchPhantomDom.dataset.startX}px`;
+      // Attach the
+      event.touchDragging = true;
+    }
+  }
+
+
+  _dragTouchEnd(event) {
+    this._touchStarted = false;
+    this._target.parentNode.removeChild(this._touchPhantomDom);
+    // Emulated dataTransfer into the event, we must attach at each touchmove so it can be retrieved in DropElement
+    event.dataTransfer = {
+      getData: type => {
+        if (type === 'text/plain') {
+          return JSON.stringify(this._data);
+        }
+      }
+    };
   }
 
 
--- front/js/utils/DropElement.js
+++ front/js/utils/DropElement.js
@@ -25,6 +25,8 @@ class DropElement {
     this._eventIds.push(Events.addEvent('dragover', this._target, this._dragOver, this));
     this._eventIds.push(Events.addEvent('dragleave', this._target, this._dragLeave, this));
     this._eventIds.push(Events.addEvent('drop', this._target, this._drop, this));
+    this._eventIds.push(Events.addEvent('touchmove', document.body, this._dragTouchOver, this));
+    this._eventIds.push(Events.addEvent('touchend', document.body, this._dragTouchEnd, this));
   }
 
 
@@ -44,6 +46,27 @@ class DropElement {
   _dragOver(event) {
     this._eventBehavior(event);
     event.dataTransfer.dropEffect = 'copy';
+  }
+
+
+  _dragTouchOver(event) {
+    if (event.touchDragging) { // This flag has been set in DragElement class to know if a touch drag is occuring
+      const rect = this._target.getBoundingClientRect();
+      const inAxisX = event.targetTouches[0].pageX >= rect.x && (event.targetTouches[0].pageX <= rect.x + rect.width);
+      const inAxisY = event.targetTouches[0].pageY >= rect.y && (event.targetTouches[0].pageY <= rect.y + rect.height);
+      if (inAxisX && inAxisY) { // Mobile equivalent to dragenter
+        this._target.style.border = 'dashed 3px rgb(255, 100, 100)';
+      } else { // Same for dragleave
+        this._target.style.border = this._borderStyle;
+      }
+    }
+  }
+
+
+  _dragTouchEnd(event) {
+    if (event.dataTransfer) { // Touch has an emulated dataTransfer element, see DragElement
+      this._drop(event);
+    }
   }
 
 
