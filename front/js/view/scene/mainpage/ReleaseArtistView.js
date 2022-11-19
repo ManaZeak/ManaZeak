8e321e4f36fc5e398c839e653f7bd09211048e4c
ArthurBeaulieu <contact@abeaulieu.fr>
2022-11-19 19:03:49 +0100

    members on rel artist

--- front/js/view/scene/mainpage/ReleaseArtistView.js
+++ front/js/view/scene/mainpage/ReleaseArtistView.js
@@ -29,6 +29,13 @@ class ReleaseArtistView extends SceneView {
   _makeInteractive() {
     return new Promise((resolve, reject) => {
       this._evtIds.push(Evts.addEvent('click', this.dom.querySelector('#artist-picture'), this._pictureClicked, this));
+      const members = this.dom.querySelector('#artist-members');
+      if (members && members.children) {
+        for (let i = 0; i < members.children.length; ++i) {
+          members.children[i].addEventListener('click', this._artistClicked);
+        }
+      }
+
       const albums = this.dom.querySelector('#released-albums');
       if (albums && albums.children) {
         for (let i = 0; i < albums.children.length; ++i) {
@@ -38,6 +45,7 @@ class ReleaseArtistView extends SceneView {
         this._scroll = new ScrollBar({
           target: albums,
           horizontal: true,
+          minSize: 200,
           style: {
             color: '#56D45B'
           }
@@ -57,6 +65,14 @@ class ReleaseArtistView extends SceneView {
       path: this.dom.querySelector('#artist-picture').children[0].children[0].children[0].src,
       artist: this.dom.querySelector('#artist-name').innerHTML
     });
+  }
+  
+
+  _artistClicked() {
+    mzk.setView({
+      name: 'ReleaseArtist',
+      id: this.dataset.id
+    });    
   }
 
 
