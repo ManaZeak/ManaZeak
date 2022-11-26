a457367e523ec2a9a823ce9b7807e6775fe31275
Squadella <pierre.bouniole@gmail.com>
2022-11-26 09:54:43 +0000

    Fixed the playback of tracks with special char inside the path.

--- back/src/main/java/org/manazeak/manazeak/controller/rest/library/track/TrackRestController.java
+++ back/src/main/java/org/manazeak/manazeak/controller/rest/library/track/TrackRestController.java
@@ -1,11 +1,11 @@
 package org.manazeak.manazeak.controller.rest.library.track;
 
+import com.google.common.escape.UnicodeEscaper;
+import com.google.common.net.PercentEscaper;
 import org.manazeak.manazeak.configuration.security.rest.RestSecurity;
 import org.manazeak.manazeak.constant.security.PrivilegeEnum;
 import org.manazeak.manazeak.entity.track.Track;
-import org.manazeak.manazeak.exception.MzkRuntimeException;
 import org.manazeak.manazeak.service.track.TrackService;
-import org.manazeak.manazeak.util.FieldUtil;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +20,7 @@ import java.net.URISyntaxException;
 @RestController
 public class TrackRestController {
 
-    private static final String TRACK_URI_PREFIX = "http:/";
+    private static final String TRACK_URI_PREFIX = "http://";
 
     private final TrackService trackService;
 
@@ -36,27 +36,28 @@ public class TrackRestController {
      */
     @RestSecurity(PrivilegeEnum.PLAY)
     @GetMapping("/play/{trackId}")
-    public ResponseEntity<Object> playTrackFromId(@PathVariable @NotNull(message = "general.error.no_id") Long trackId) {
-        try {
-            Track track = trackService.getTrackById(trackId);
+    public ResponseEntity<Object> playTrackFromId(@PathVariable @NotNull(message = "general.error.no_id") Long trackId) throws URISyntaxException {
+        // Getting the track for the id.
+        Track track = trackService.getTrackById(trackId);
 
+        // Formatting the location with a percent escaper.
+        UnicodeEscaper basicEscaper = new PercentEscaper("/", false);
+        String locationEscaped = basicEscaper.escape(track.getLocation());
+        String urlEscaped = basicEscaper.escape(TRACK_URI_PREFIX + track.getLocation());
 
-            String trackLocation = FieldUtil.formatToUrl(track.getLocation());
-            URI url = new URI(TRACK_URI_PREFIX + trackLocation);
-            HttpHeaders header = new HttpHeaders();
-            header.setLocation(url);
-            if (Boolean.TRUE.equals(track.getIsMp3())) {
-                header.set("Content-Type", "audio/mpeg");
-            } else {
-                header.set("Content-Type", "audio/flac");
-            }
-            header.set("Content-Disposition", "inline");
-            header.set("X-Accel-Redirect", trackLocation);
-
-            return new ResponseEntity<>(header, HttpStatus.SEE_OTHER);
-        } catch (URISyntaxException e) {
-            throw new MzkRuntimeException("The location of the track cannot be used for an URI.", e);
+        // Preparing the redirection to the nginx to serve the track.
+        HttpHeaders header = new HttpHeaders();
+        header.setLocation(new URI(urlEscaped));
+        if (Boolean.TRUE.equals(track.getIsMp3())) {
+            header.set("Content-Type", "audio/mpeg");
+        } else {
+            header.set("Content-Type", "audio/flac");
         }
+        header.set("Content-Disposition", "inline");
+        header.set("X-Accel-Redirect", locationEscaped);
+
+        // Sending the response to the client.
+        return new ResponseEntity<>(header, HttpStatus.SEE_OTHER);
     }
 
 }
