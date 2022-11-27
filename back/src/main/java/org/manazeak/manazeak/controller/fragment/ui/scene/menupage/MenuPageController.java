873bc46e08a6edfa14b5e6c9a364cbd7564e78d1
Squadella <pierre.bouniole@gmail.com>
2022-11-27 19:58:13 +0100

    Removed the admin button, if the user is not an admin.

--- back/src/main/java/org/manazeak/manazeak/controller/fragment/ui/scene/menupage/MenuPageController.java
+++ back/src/main/java/org/manazeak/manazeak/controller/fragment/ui/scene/menupage/MenuPageController.java
@@ -1,9 +1,11 @@
 package org.manazeak.manazeak.controller.fragment.ui.scene.menupage;
 
 import org.manazeak.manazeak.configuration.security.Security;
+import org.manazeak.manazeak.configuration.security.SecurityUtil;
 import org.manazeak.manazeak.constant.security.PrivilegeEnum;
 import org.manazeak.manazeak.controller.fragment.FragmentController;
 import org.manazeak.manazeak.controller.page.ui.UiFragmentEnum;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -14,7 +16,8 @@ public class MenuPageController {
 
     @Security(PrivilegeEnum.PLAY)
     @GetMapping("/menupage")
-    public String getMenuPage() {
+    public String getMenuPage(Model model) {
+        model.addAttribute("isAdmin", SecurityUtil.currentUserHasPrivilege(PrivilegeEnum.ADMV));
         return UiFragmentEnum.MENU_PAGE.getPage();
     }
 }
