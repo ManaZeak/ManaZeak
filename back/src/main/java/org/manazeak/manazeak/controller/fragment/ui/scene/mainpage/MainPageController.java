85b733a81d1f83df1b0c9d2f3cd39044e04f462b
Pierre Bouniol <pierre.bouniol@3ds.com>
2022-12-16 14:33:51 +0100

    Added a new way to display the main page with a custom number of elements.

--- back/src/main/java/org/manazeak/manazeak/controller/fragment/ui/scene/mainpage/MainPageController.java
+++ back/src/main/java/org/manazeak/manazeak/controller/fragment/ui/scene/mainpage/MainPageController.java
@@ -1,5 +1,8 @@
 package org.manazeak.manazeak.controller.fragment.ui.scene.mainpage;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
 import org.manazeak.manazeak.configuration.security.Security;
 import org.manazeak.manazeak.constant.security.PrivilegeEnum;
 import org.manazeak.manazeak.controller.fragment.FragmentController;
@@ -8,6 +11,7 @@ import org.manazeak.manazeak.service.library.artist.ArtistService;
 import org.manazeak.manazeak.service.library.genre.GenreService;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 /**
@@ -28,10 +32,29 @@ public class MainPageController {
     @Security(PrivilegeEnum.PLAY)
     @GetMapping("/mainpage/")
     public String getMainPage(Model model) {
-        // Adding the artists
-        model.addAttribute("rlArtists", artistService.getSomeRandomArtistMinimal(NB_ELEMENTS));
-        model.addAttribute("genres", genreService.getSomeRandomGenreMinimal(NB_ELEMENTS));
+        addMainPageAttributes(NB_ELEMENTS, model);
 
         return UiFragmentEnum.MAIN_PAGE.getPage();
+    }
+
+    /**
+     * Return the main page fragment with the number element specified.
+     * No more than 50 element per category can be asked.
+     *
+     * @param numberOfElement The number of element to return in each section.
+     * @param model         The model to pass to thymeleaf to fill the template.
+     * @return The address of the page to use.
+     */
+    @Security(PrivilegeEnum.PLAY)
+    @GetMapping("/mainpage/{numberOfElement}")
+    public String getMainPageWithNumberOfElements(@PathVariable @Valid @NotNull @Max(50) Integer numberOfElement, Model model) {
+        addMainPageAttributes(numberOfElement, model);
+
+        return UiFragmentEnum.MAIN_PAGE.getPage();
+    }
+
+    private void addMainPageAttributes(int numberOfElements, Model model) {
+        model.addAttribute("rlArtists", artistService.getSomeRandomArtistMinimal(numberOfElements));
+        model.addAttribute("genres", genreService.getSomeRandomGenreMinimal(numberOfElements));
     }
 }
