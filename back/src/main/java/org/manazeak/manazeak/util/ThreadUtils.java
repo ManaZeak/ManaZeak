49794ed62ebb87bb7e747fbdd5b9fd4b10717960
Squadella <pierre.bouniole@gmail.com>
2024-05-12 10:53:49 +0200

    #286 Code smells fix in the MoodbarGenerationService.
    The admin page now has understandable buttons.

---
+++ back/src/main/java/org/manazeak/manazeak/util/ThreadUtils.java
@@ -0,0 +1,39 @@
+package org.manazeak.manazeak.util;
+
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.manazeak.manazeak.exception.MzkRuntimeException;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public final class ThreadUtils {
+
+    private ThreadUtils() {
+    }
+
+    /**
+     * Wait for the executor to finish all thread contained.
+     *
+     * @param executor The executor service to wait.
+     */
+    public static void awaitExecutorTermination(@NonNull final ExecutorService executor, @NonNull String taskName) {
+        // Checking if the executor is terminated.
+        if (!executor.isTerminated()) {
+            log.error("The executor must be terminated, otherwise this method will forever.");
+            throw new MzkRuntimeException("Executor is not terminated");
+        }
+
+        // Waiting for the threads to finish.
+        try {
+            if (!executor.awaitTermination(Long.MAX_VALUE, TimeUnit.SECONDS)) {
+                throw new MzkRuntimeException("The timeout for the " + taskName);
+            }
+        } catch (InterruptedException e) {
+            log.error("Thread interrupted during the {}.", taskName, e);
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
