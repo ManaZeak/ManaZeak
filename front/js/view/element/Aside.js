960fe6070cfdb7987773d493d133a0f37c193f30
ArthurBeaulieu <arthur@beaulieu.email>
2025-12-12 21:33:57 +0100

    fix: missing tooltip use in responsive files

--- front/js/view/element/Aside.js
+++ front/js/view/element/Aside.js
@@ -3,6 +3,7 @@ class Aside {
 
   constructor() {
     this._homepage = document.getElementById('homepage-button');
+    this._newplaylist = document.getElementById('newplaylist-button');
     this._menupage = document.getElementById('menupage-button');
     this._logout = document.getElementById('logout-button');
     this._evtIds = [];
@@ -16,6 +17,7 @@ class Aside {
 
   _events() {
     this._evtIds.push(Evts.addEvent('click', this._homepage, mzk.setView.bind(mzk, { name: 'MainPage' }), this));
+    this._evtIds.push(Evts.addEvent('click', this._newplaylist, mzk.setModal.bind(mzk, { name: 'NewPlaylist' }), this));
     this._evtIds.push(Evts.addEvent('click', this._menupage, mzk.setView.bind(mzk, { name: 'MenuPage' }), this));
     this._evtIds.push(Evts.addEvent('click', this._logout, this._logoutSession, this));
   }
